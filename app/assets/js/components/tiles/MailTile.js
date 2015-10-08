a1cfbe21ed805c079feed72a9709eda39e48a92a
Alec Cursley <a.cursley@warwick.ac.uk>
2015-10-08 18:05:12 +0100

    Connect the tile to a Flux-y dispatcher and store

--- app/assets/js/components/tiles/MailTile.js
+++ app/assets/js/components/tiles/MailTile.js
@@ -3,30 +3,84 @@ const ReactComponent = require('react/lib/ReactComponent');
 
 const Tile = require('./Tile');
 
+const Dispatcher = require('../../Dispatcher');
+const FluxStore = require('flux/lib/FluxStore');
+
+const Immutable = require('immutable');
+
+var messages = Immutable.List();
+
+class MailStore extends FluxStore {
+
+    getMessages() {
+        return messages;
+    }
+
+    __onDispatch(action) {
+        switch (action.type) {
+            case 'mail-receive':
+                messages = messages.unshift(action.mail);
+                this.__emitChange();
+                break;
+            default:
+            // no-op
+        }
+    }
+
+}
+
+let Store = new MailStore(Dispatcher);
+
+class MailActions {
+
+    static didRecieveMail(mail) {
+        Dispatcher.dispatch({
+            type: 'mail-receive',
+            mail: mail
+        });
+    }
+
+}
+
+MailActions.didRecieveMail({
+    id: 1,
+    from: 'Squirrell, Linda',
+    subject: 'IT Induction Day - Reminder',
+    date: 'Monday'
+});
+
+setTimeout(() => {
+    MailActions.didRecieveMail({
+        id: 2,
+        from: 'Kennedy, Christelle',
+        subject: 'Departmental meeting',
+        date: 'Yesterday'
+    });
+}, 1000);
+
+setTimeout(() => {
+    MailActions.didRecieveMail({
+        id: 3,
+        from: 'IT Service Desk',
+        subject: 'Emergency RFC0021650 - Network',
+        date: '09:48'
+    });
+}, 2000);
+
 export default class MailTile extends ReactComponent {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            unreadMessages: [
-                {
-                    from: 'IT Service Desk',
-                    subject: 'Emergency RFC0021650 - Network',
-                    date: '09:48'
-                },
-                {
-                    from: 'Kennedy, Christelle',
-                    subject: 'Departmental meeting',
-                    date: 'Yesterday'
-                },
-                {
-                    from: 'Squirrell, Linda',
-                    subject: 'IT Induction Day - Reminder',
-                    date: 'Monday'
-                }
-            ]
+            messages: Store.getMessages()
         };
+
+        Store.addListener(() => {
+            this.setState({
+                messages: Store.getMessages()
+            });
+        });
     }
 
     render() {
@@ -35,14 +89,14 @@ export default class MailTile extends ReactComponent {
                 return (
                     <Tile title="Mail" href="http://warwick.ac.uk/mymail">
                         <ul className="list-unstyled">
-                            {this.state.unreadMessages.map((message) => <MailMessageListItem {...message} />)}
+                            {this.state.messages.map((message) => <MailMessageListItem key={message.id} {...message} />)}
                         </ul>
                     </Tile>
                 );
             default:
                 return (
                     <Tile title="Mail" href="http://warwick.ac.uk/mymail">
-                        {this.state.unreadMessages.length} unread
+                        {this.state.messages.length} unread
                     </Tile>
                 );
         }
