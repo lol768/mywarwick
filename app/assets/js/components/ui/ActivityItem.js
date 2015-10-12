396712c4c9b8de963823ce2715bfbbe5688f91e8
Alec Cursley <a.cursley@warwick.ac.uk>
2015-10-12 17:06:16 +0100

    Add AppIcon component and use in ActivityItem

--- app/assets/js/components/ui/ActivityItem.js
+++ app/assets/js/components/ui/ActivityItem.js
@@ -3,14 +3,23 @@ const ReactComponent = require('react/lib/ReactComponent');
 
 const moment = require('moment');
 
+const AppIcon = require('./AppIcon');
+
 export default class ActivityItem extends ReactComponent {
 
     render() {
         return (
             <div className="activity-item">
-                <span className="activity-item__text">{this.props.text}</span>
-                <span className="activity-item__source">{this.props.source}</span>
-                <span className="activity-item__date">{moment(this.props.date).calendar()}</span>
+                <div className="media">
+                    <div className="media-left">
+                        <AppIcon app={this.props.source} size="lg" />
+                    </div>
+                    <div className="media-body">
+                        <span className="activity-item__text">{this.props.text}</span>
+                        <span className="activity-item__source">{this.props.source}</span>
+                        <span className="activity-item__date">{moment(this.props.date).calendar()}</span>
+                    </div>
+                </div>
             </div>
         );
     }
