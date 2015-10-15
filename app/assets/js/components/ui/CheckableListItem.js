cad11161536dc72bdd89f2fb817662dcb6719fa9
Alec Cursley <a.cursley@warwick.ac.uk>
2015-10-15 15:58:02 +0100

    Tidy up the News tab; some UI to select sources

---
+++ app/assets/js/components/ui/CheckableListItem.js
@@ -0,0 +1,37 @@
+const React = require('react');
+const ReactComponent = require('react/lib/ReactComponent');
+
+export default class CheckableListItem extends ReactComponent {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            checked: this.props.checked || false
+        };
+    }
+
+    toggle(e) {
+        e.preventDefault();
+
+        this.setState({
+            checked: !this.state.checked
+        });
+    }
+
+    render() {
+        return (
+            <a href="#" className="list-group-item" onClick={this.toggle.bind(this)}>
+                <span className="media">
+                    <span className="media-left">
+                        <i className={"fa fa-fw fa-" + (this.state.checked ? 'check-square' : 'square')} style={{color: this.props.color}}></i>
+                    </span>
+                    <span className="media-body">
+                        {this.props.text}
+                    </span>
+                </span>
+            </a>
+        );
+    }
+
+}
