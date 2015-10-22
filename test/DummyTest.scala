37923579651e7827237127bd89e565909c48cfa5
Alec Cursley <a.cursley@warwick.ac.uk>
2015-10-22 16:03:26 +0100

    Add a dummy test so we can build in impunity

---
+++ test/DummyTest.scala
@@ -0,0 +1,8 @@
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+
+class DummyTest extends Specification {
+
+  "The build" should {"not fail" in new Scope {"Bamboo"}}
+
+}
