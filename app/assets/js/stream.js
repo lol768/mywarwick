9ffef2b25d92aa71c8846baecba7e27f2fd15b08
Alec Cursley <alec@cursley.net>
2015-10-27 14:29:03 +0000

    Add Stream class with item partitioning

--- app/assets/js/stream.js
+++ app/assets/js/stream.js
@@ -10,43 +10,112 @@ let sortStream = (stream) => _.sortByOrder(stream, [DATE_KEY, ID_KEY], [DESC, DE
 
 let uniqStream = (stream) => _.uniq(stream, ID_KEY);
 
+export class Stream {
+
+  /*
+   * Takes a partition function that is used to group stream items into
+   * buckets, to make updates and persistence more efficient for streams
+   * with a large number of items.
+   *
+   * The result of the partition function has to be a valid object key, and
+   * be sortable. Aside from that, it doesn't really matter what it is, as
+   * long as it's the same for all items that belong in the same partition.
+   */
+  constructor(partitionFn) {
+    this.partitionFn = partitionFn;
+    this.partitions = {};
+    this.sortedPartitionKeys = [];
+  }
+
+  /*
+   * Merge a list of received items into this Stream.
+   */
+  receive(rx) {
+    let beforeCount = this._privatePartitionCount();
+
+    _(rx).groupBy(this.partitionFn).each((items, key) => {
+      this.partitions[key] = onReceive(this.partitions[key], items);
+    }).value();
+
+    // If any new partitions were added, re-sort the partition keys for
+    // index-based access
+    if (this._privatePartitionCount() != beforeCount) {
+      this.sortedPartitionKeys = Object.keys(this.partitions).sort();
+    }
+  }
+
+  /*
+   * This is pseudo-private because apparently Object.keys() runs in linear
+   * time on the size of the object...
+   */
+  _privatePartitionCount() {
+    return Object.keys(this.partitions).length;
+  }
+
+  /*
+   * ... whereas Array.length is constant time.
+   */
+  partitionCount() {
+    return this.sortedPartitionKeys.length;
+  }
+
+  /*
+   * Get the items in the stream partition at the given index.  Indexes are
+   * determined by sorting the partition keys.
+   */
+  getPartition(i) {
+    let key = this.sortedPartitionKeys[i];
+
+    return this.partitions[key];
+  }
+
+}
+
+/*
+ * Add the items in rx to the stream.  The resulting array is ordered on date
+ * descending, and is free of duplicate items.
+ *
+ * A number of tricks are attempted to reduce the amount of work required to
+ * perform the merge.  If these all fail, it falls back to concatenating,
+ * de-duplicating and sorting the whole thing.
+ */
 export function onReceive(stream = [], rx = []) {
-    // Preconditions: stream has no duplicates, stream is in reverse date order
+  // Preconditions: stream has no duplicates, stream is in reverse date order
 
-    // Defensive copy both
-    stream = _.cloneDeep(stream);
-    rx = uniqStream(rx);
+  // Defensive copy both
+  stream = _.cloneDeep(stream);
+  rx = uniqStream(rx);
 
-    let newest = _(stream).first();
-    let oldestRx = _(rx).min((x) => x[DATE_KEY]);
+  let newest = _(stream).first();
+  let oldestRx = _(rx).min((x) => x[DATE_KEY]);
 
-    // Short circuit if existing stream is empty
-    if (stream.length == 0) {
-        return sortStream(rx);
-    }
+  // Short circuit if existing stream is empty
+  if (stream.length == 0) {
+    return sortStream(rx);
+  }
 
-    // Short circuit if all received things are newer than the newest we have
-    if (newest[DATE_KEY] < oldestRx[DATE_KEY]) {
-        stream.unshift(...sortStream(rx));
+  // Short circuit if all received things are newer than the newest we have
+  if (newest[DATE_KEY] < oldestRx[DATE_KEY]) {
+    stream.unshift(...sortStream(rx));
 
-        return uniqStream(stream);
-    }
+    return uniqStream(stream);
+  }
 
-    // Do the smallest possible merge
-    // >= to include identical items in dedupe later
-    let mergeStart = _(stream).findLastIndex((x) => x[DATE_KEY] >= oldestRx[DATE_KEY]);
+  // Do the smallest possible merge
+  // >= to include identical items in dedupe later
+  let mergeStart = _(stream).findLastIndex((x) => x[DATE_KEY] >= oldestRx[DATE_KEY]);
 
-    if (mergeStart == -1) {
-        // If all rx items older than all stream items, merge whole array
-        stream.push(...rx);
+  if (mergeStart == -1) {
+    // If all rx items older than all stream items, merge whole array
+    stream.push(...rx);
 
-        return sortStream(uniqStream(stream));
-    } else {
-        let toMerge = stream.splice(0, mergeStart + 1);
-        toMerge.push(...rx);
+    return sortStream(uniqStream(stream));
+  } else {
+    let toMerge = stream.splice(0, mergeStart + 1);
+    toMerge.push(...rx);
 
-        stream.unshift(...sortStream(uniqStream(toMerge)));
+    stream.unshift(...sortStream(uniqStream(toMerge)));
 
-        return stream;
-    }
+    return stream;
+  }
 }
--- test/assets/js/StreamTest.js
+++ test/assets/js/StreamTest.js
@@ -1,69 +1,99 @@
-import { onReceive } from '../../../app/assets/js/stream';
+import { Stream, onReceive } from '../../../app/assets/js/stream';
 
-describe('stream', () => {
+let item = (id, date) => ({id: id, date: date});
 
-    let item = (id, date) => ({id: id, date: date});
+describe('Stream', () => {
 
-    it('initially sorts items', () => {
-        let stream = onReceive([], [item('a', 1), item('b', 2)]);
+  it('partitions received items', () => {
 
-        expect(stream.map((i) => i.id)).to.eql(['b', 'a']);
-    });
+    let stream = new Stream((x) => Math.floor(x.date / 10))
 
-    it('adds first item', () => {
-        let stream = onReceive([], [item('a', 1)]);
+    stream.receive([item('a', 1)]);
 
-        expect(stream.length).to.equal(1);
-    });
+    expect(stream.partitionCount()).to.equal(1);
+    expect(stream.getPartition(0)).to.eql([item('a', 1)]);
+    expect(stream.getPartition(1)).to.equal(undefined);
 
-    it('prepends a newer item', () => {
-        let stream = onReceive([item('a', 1)], [item('b', 2)]);
+  });
 
-        expect(stream.length).to.equal(2);
-        expect(stream[0].id).to.equal('b');
-    });
+  it('imposes an ordering on partition keys', () => {
 
-    it('adds items where all are newer than existing', () => {
-        let stream1 = onReceive([
-            item('b', 2),
-            item('a', 1)
-        ], [
-            item('c', 3),
-            item('e', 5),
-            item('d', 4)
-        ]);
+    let stream = new Stream((x) => Math.floor(x.date / 10))
 
-        let stream2 = onReceive([
-            item('e', 5),
-            item('d', 4),
-            item('b', 2),
-            item('a', 1)
-        ], [
-            item('c', 3)
-        ]);
+    stream.receive([item('a', 1), item('b', 11), item('c', 21)]);
 
-        let stream3 = onReceive([
-            item('d', 4)
-        ], [
-            item('c', 3),
-            item('e', 5),
-            item('b', 2),
-            item('a', 1)
-        ]);
+    expect(stream.partitionCount()).to.equal(3);
+    expect(stream.getPartition(0)[0].date).to.equal(1);
+    expect(stream.getPartition(1)[0].date).to.equal(11);
+    expect(stream.getPartition(2)[0].date).to.equal(21);
 
-        let stream4 = onReceive([
-            item('e', 5),
-            item('d', 4)
-        ], [
-            item('c', 3),
-            item('b', 2),
-            item('a', 1)
-        ]);
-
-        expect(stream1).to.eql(stream2);
-        expect(stream1).to.eql(stream3);
-        expect(stream1).to.eql(stream4);
-
-    });
+  });
 
 });
+
+describe('onReceive', () => {
+
+  it('initially sorts items', () => {
+    let stream = onReceive([], [item('a', 1), item('b', 2)]);
+
+    expect(stream.map((i) => i.id)).to.eql(['b', 'a']);
+  });
+
+  it('adds first item', () => {
+    let stream = onReceive([], [item('a', 1)]);
+
+    expect(stream.length).to.equal(1);
+  });
+
+  it('prepends a newer item', () => {
+    let stream = onReceive([item('a', 1)], [item('b', 2)]);
+
+    expect(stream.length).to.equal(2);
+    expect(stream[0].id).to.equal('b');
+  });
+
+  it('adds items where all are newer than existing', () => {
+    let stream1 = onReceive([
+      item('b', 2),
+      item('a', 1)
+    ], [
+      item('c', 3),
+      item('e', 5),
+      item('d', 4)
+    ]);
+
+    let stream2 = onReceive([
+      item('e', 5),
+      item('d', 4),
+      item('b', 2),
+      item('a', 1)
+    ], [
+      item('c', 3)
+    ]);
+
+    let stream3 = onReceive([
+      item('d', 4)
+    ], [
+      item('c', 3),
+      item('e', 5),
+      item('b', 2),
+      item('a', 1)
+    ]);
+
+    let stream4 = onReceive([
+      item('e', 5),
+      item('d', 4)
+    ], [
+      item('c', 3),
+      item('b', 2),
+      item('a', 1)
+    ]);
+
+    expect(stream1).to.eql(stream2);
+    expect(stream1).to.eql(stream3);
+    expect(stream1).to.eql(stream4);
+
+  });
+
+});
+
