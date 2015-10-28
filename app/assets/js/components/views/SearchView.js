58f6898a907c1898cf4cc64341fad45d2582b5c8
nicduke <n.duke@warwick.ac.uk>
2015-10-28 13:13:14 +0000

    NEWSTART-23 removed React onBlur handler and added listener in the onSearchFocus function that checks subsequent click event is not inside the recentLinks element. This looks messy; there's probably a nicer way

--- app/assets/js/components/views/SearchView.js
+++ app/assets/js/components/views/SearchView.js
@@ -7,60 +7,69 @@ import Link from '../ui/Link';
 
 export default class SearchView extends ReactComponent {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            query: ''
-        };
-    }
+    this.state = {
+      query: ''
+    };
+  }
 
-    onSearchChange(text) {
-        this.setState({
-            query: text
-        });
-    }
+  onSearchChange(text) {
+    this.setState({
+      query: text
+    });
+  }
 
-    onSearchFocus() {
-        this.setState({
-            searchFocus: true
-        });
-    }
+  onSearchFocus() {
 
-    onSearchBlur() {
-        this.setState({
-            searchFocus: false
-        });
-    }
+    var _this = this;
 
-    render() {
-        let links = (
-            <div style={{marginTop: '20px'}}>
-                <h4>Quick links</h4>
-                <LinkBlock columns="3">
-                    <Link key="bpm" href="http://warwick.ac.uk/bpm">Course Transfers</Link>
-                    <Link key="ett" href="http://warwick.ac.uk/ett">Exam Timetable</Link>
-                    <Link key="massmail" href="http://warwick.ac.uk/massmail">Mass Mailing</Link>
+    _this.setState({
+      searchFocus: true
+    });
 
-                    <Link key="mrm" href="http://warwick.ac.uk/mrm">Module Registration</Link>
-                    <Link key="printercredits" href="http://warwick.ac.uk/printercredits">Printer Credits</Link>
-                </LinkBlock>
-            </div>
-        );
+    $(document).bind('click.searchBox', function (e) {
+      if ($(e.target).closest('.recentLinks').length || $(e.target).hasClass('form-control')) return;
+      _this.setState({
+        searchFocus: false
+      });
+      $(document).unbind('.searchBox');
+    })
 
-        let suggestions = (
-            <LinkBlock columns="1">
-                <Link key="a" href="http://">Recent 1</Link>
-                <Link key="b" href="http://">Recent 2</Link>
-                <Link key="c" href="http://">Recent 3</Link>
-            </LinkBlock>
-        );
+  }
 
-        return (
-            <div>
-                <SearchField value={this.state.query} onChange={this.onSearchChange.bind(this)} onFocus={this.onSearchFocus.bind(this)} onBlur={this.onSearchBlur.bind(this)} />
-                {this.state.searchFocus ? suggestions : links}
-            </div>
-        );
-    }
+  render() {
+    let links = (
+      <div style={{marginTop: '20px'}}>
+        <h4>Quick links</h4>
+        <LinkBlock columns="3">
+          <Link key="bpm" href="http://warwick.ac.uk/bpm">Course Transfers</Link>
+          <Link key="ett" href="http://warwick.ac.uk/ett">Exam Timetable</Link>
+          <Link key="massmail" href="http://warwick.ac.uk/massmail">Mass Mailing</Link>
+
+          <Link key="mrm" href="http://warwick.ac.uk/mrm">Module Registration</Link>
+          <Link key="printercredits" href="http://warwick.ac.uk/printercredits">Printer Credits</Link>
+        </LinkBlock>
+      </div>
+    );
+
+    let suggestions = (
+      <div className="recentLinks">
+        <LinkBlock columns="1">
+          <Link key="a" href="http://">Recent 1</Link>
+          <Link key="b" href="http://">Recent 2</Link>
+          <Link key="c" href="http://">Recent 3</Link>
+        </LinkBlock>
+      </div>
+    );
+
+    return (
+      <div>
+        <SearchField value={this.state.query} onChange={this.onSearchChange.bind(this)}
+                     onFocus={this.onSearchFocus.bind(this)}/>
+        {this.state.searchFocus ? suggestions : links}
+      </div>
+    );
+  }
 }
