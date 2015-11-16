41ea872cbbf0d7f58c38c9dc139ff8a2268cbcbd
Alec Cursley <a.cursley@warwick.ac.uk>
2015-11-16 16:01:55 +0000

    Add ActivityTargetServiceTest

---
+++ test/helpers/TestObjectFactory.scala
@@ -0,0 +1,54 @@
+package helpers
+
+import org.joda.time.DateTime
+import services.{Group, GroupName}
+import warwick.sso.{Department, Name, User, Usercode}
+
+object TestObjectFactory {
+
+  def makeGroup(name: String = "in-elab", members: Seq[String] = Seq("a", "b")): Group =
+    Group(
+      GroupName(name),
+      Some("ITS web team"),
+      members.map(Usercode),
+      Seq(Usercode("a")),
+      "Arbitrary",
+      Department(None, Some("Information Technology Services"), Some("in")),
+      DateTime.now()
+    )
+
+  def makeFoundUser(usercode: String = "user"): User =
+    User(
+      usercode = Usercode(usercode),
+      universityId = None,
+      name = Name(None, None),
+      email = None,
+      department = None,
+      isStaffOrPGR = false,
+      isStaffNotPGR = false,
+      isStudent = true,
+      isAlumni = false,
+      isFound = true,
+      isVerified = true,
+      isLoginDisabled = false,
+      rawProperties = Map.empty
+    )
+
+  def makeNotFoundUser(usercode: String = "user"): User =
+    User(
+      usercode = Usercode(usercode),
+      universityId = None,
+      name = Name(None, None),
+      email = None,
+      department = None,
+      isStaffOrPGR = false,
+      isStaffNotPGR = false,
+      isStudent = true,
+      isAlumni = false,
+      isFound = false,
+      isVerified = true,
+      isLoginDisabled = false,
+      rawProperties = Map.empty
+    )
+
+}
