879b4295f6d015b066a4d578d4fba04bf08a9d62
Alec Cursley <a.cursley@warwick.ac.uk>
2015-11-26 15:14:34 +0000

    Use DAOs instead of SQL INSERTs in test

--- test/services/ActivityRecipientDaoTest.scala
+++ test/services/ActivityRecipientDaoTest.scala
@@ -3,10 +3,11 @@ package services
 import anorm.SQL
 import anorm.SqlParser._
 import helpers.OneStartAppPerSuite
+import helpers.TestObjectFactory._
 import org.joda.time.DateTime
 import org.scalatestplus.play.PlaySpec
 import play.api.db.Database
-import services.dao.ActivityRecipientDaoImpl
+import services.dao.{ActivityDao, ActivityRecipientDao}
 import warwick.anorm.converters.ColumnConversions._
 
 class ActivityRecipientDaoTest extends PlaySpec with OneStartAppPerSuite {
@@ -14,27 +15,18 @@ class ActivityRecipientDaoTest extends PlaySpec with OneStartAppPerSuite {
   val db = app.injector.instanceOf[Database]
   implicit val c = db.getConnection()
 
-  val activityRecipientDao = new ActivityRecipientDaoImpl(db)
+  val activityDao = app.injector.instanceOf[ActivityDao]
+  val activityRecipientDao = app.injector.instanceOf[ActivityRecipientDao]
 
-  SQL(
-    """
-       INSERT INTO ACTIVITY (ID, PROVIDER_ID, TYPE, TITLE, TEXT, GENERATED_AT, CREATED_AT, SHOULD_NOTIFY)
-       VALUES ('abc', 'tabula', 'due', 'Coursework due', 'Your coursework is due in 7 days', SYSDATE, SYSDATE, 1)
-    """).execute()
+  val activityId = activityDao.save(makeActivityPrototype(), Seq.empty)
 
-  SQL(
-    """
-         INSERT INTO ACTIVITY_RECIPIENT (ACTIVITY_ID, USERCODE, CREATED_AT, GENERATED_AT) VALUES
-         ('abc', 'someone', SYSDATE, SYSDATE)
-    """).execute()
-  implicit val c = db.getConnection()
   "ActivityRecipientDao" should {
 
     "create a recipient" in {
 
-      activityRecipientDao.create("abc", "other", None)
+      activityRecipientDao.create(activityId, "someone", None)
 
-      val count = SQL("SELECT COUNT(*) FROM ACTIVITY_RECIPIENT WHERE ACTIVITY_ID = 'abc' AND USERCODE = 'other'").as(scalar[Int].single)
+      val count = SQL("SELECT COUNT(*) FROM ACTIVITY_RECIPIENT WHERE ACTIVITY_ID = {activityId} AND USERCODE = 'someone'").on('activityId -> activityId).as(scalar[Int].single)
 
       count must be(1)
 
@@ -42,9 +34,9 @@ class ActivityRecipientDaoTest extends PlaySpec with OneStartAppPerSuite {
 
     "mark an activity as sent" in {
 
-      activityRecipientDao.markSent("abc", "someone")
+      activityRecipientDao.markSent(activityId, "someone")
 
-      val date = SQL("SELECT SENT_AT FROM ACTIVITY_RECIPIENT WHERE ACTIVITY_ID = 'abc' AND USERCODE = 'someone'").as(scalar[DateTime].singleOpt)
+      val date = SQL("SELECT SENT_AT FROM ACTIVITY_RECIPIENT WHERE ACTIVITY_ID = {activityId} AND USERCODE = 'someone'").on('activityId -> activityId).as(scalar[DateTime].singleOpt)
 
       date must not be None
 
