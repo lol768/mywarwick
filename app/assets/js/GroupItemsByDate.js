ac27635ccf5cbf200870c015e680c23ef35f8792
Alec Cursley <a.cursley@warwick.ac.uk>
2015-12-16 17:26:11 +0000

    Fix GroupItemsByDate today() in browser

--- app/assets/js/GroupItemsByDate.js
+++ app/assets/js/GroupItemsByDate.js
@@ -1,46 +1,41 @@
 import moment from 'moment';
 
 // A way to describe a grouping strategy
-let groupItemsByDate = {
 
-  // Describe how things are being grouped
-  // Exposed as a CSS class on the grouped list
-  description: 'by-date',
+// Describe how things are being grouped
+// Exposed as a CSS class on the grouped list
+export const description = 'by-date';
 
-  today() {
-    return moment();
-  },
+export function today() {
+  return moment();
+}
 
-  // Which group an item belongs in
-  // Return an arbitrary identifier that is the same for all items in the same group
-  groupForItem(item) {
-    var date = moment(item.props.date).startOf('day');
+// Which group an item belongs in
+// Return an arbitrary identifier that is the same for all items in the same group
+export function groupForItem(item) {
+  var date = moment(item.props.date).startOf('day');
 
-    if (date.isSame(this.today(), 'day')) {
-      return 0;
-    } else if (date.isSame(this.today().subtract(1, 'day'), 'day')) {
-      return 1;
-    } else if (date.isSame(this.today(), 'isoWeek')) {
-      return 2;
-    } else if (date.isSame(this.today().subtract(1, 'week'), 'isoWeek')) {
-      return 3;
-    } else {
-      return 4;
-    }
-  },
-
-  // The title to be displayed for items in the group
-  // Return a nice title for the user to look at, from the group identifier
-  titleForGroup(group) {
-    return [
-      'Today',
-      'Yesterday',
-      'This Week',
-      'Last Week',
-      'Older'
-    ][group];
+  if (date.isSame(today(), 'day')) {
+    return 0;
+  } else if (date.isSame(today().subtract(1, 'day'), 'day')) {
+    return 1;
+  } else if (date.isSame(today(), 'isoWeek')) {
+    return 2;
+  } else if (date.isSame(today().subtract(1, 'week'), 'isoWeek')) {
+    return 3;
+  } else {
+    return 4;
   }
+}
 
-};
-
-export default groupItemsByDate;
+// The title to be displayed for items in the group
+// Return a nice title for the user to look at, from the group identifier
+export function titleForGroup(group) {
+  return [
+    'Today',
+    'Yesterday',
+    'This Week',
+    'Last Week',
+    'Older'
+  ][group];
+}
