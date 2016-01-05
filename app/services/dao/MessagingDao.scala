e3692e228c7b6add6f919399484be8f78294661d
Alec Cursley <a.cursley@warwick.ac.uk>
2016-01-05 16:06:22 +0000

    Extract DAO methods from MessagingService

--- app/services/dao/MessagingDao.scala
+++ app/services/dao/MessagingDao.scala
@@ -1,20 +1,52 @@
 package services.dao
 
 import java.sql.Connection
+import java.util.UUID
 
-import anorm.SQL
+import anorm._
 import com.google.inject.ImplementedBy
+import models.Activity
 import org.joda.time.DateTime
+import services.messaging.{MessageSend, Output}
 import warwick.anorm.converters.ColumnConversions._
+import warwick.sso.Usercode
 
 @ImplementedBy(classOf[MessagingDaoImpl])
 trait MessagingDao {
 
+  def save(activity: Activity, usercode: Usercode, output: Output)(implicit c: Connection): Unit
+
+  def complete(messageId: String, state: String)(implicit c: Connection): Unit
+
   def deleteMessagesSuccessfullySentBefore(date: DateTime)(implicit c: Connection): Int
+
+  def lockRecord()(implicit c: Connection): Option[MessageSend.Light]
 
 }
 
 class MessagingDaoImpl extends MessagingDao {
+
+  override def save(activity: Activity, usercode: Usercode, output: Output)(implicit c: Connection): Unit = {
+    SQL("INSERT INTO MESSAGE_SEND (ID, ACTIVITY_ID, USERCODE, OUTPUT, UPDATED_AT) VALUES ({id}, {activityId}, {usercode}, {output}, {updatedAt})")
+      .on(
+        'id -> UUID.randomUUID().toString,
+        'activityId -> activity.id,
+        'usercode -> usercode.string,
+        'output -> output.name,
+        'updatedAt -> DateTime.now
+      )
+      .execute()
+  }
+
+  override def complete(messageId: String, state: String)(implicit c: Connection): Unit = {
+    SQL("UPDATE MESSAGE_SEND SET STATE = {state}, UPDATED_AT = {updatedAt} WHERE ID = {id}")
+      .on(
+        'id -> messageId,
+        'state -> state,
+        'updatedAt -> DateTime.now
+      )
+      .execute()
+  }
 
   override def deleteMessagesSuccessfullySentBefore(date: DateTime)(implicit c: Connection): Int = {
     SQL("DELETE FROM MESSAGE_SEND WHERE STATE = {state} AND UPDATED_AT < {date}")
@@ -23,6 +55,25 @@ class MessagingDaoImpl extends MessagingDao {
         'date -> date
       )
       .executeUpdate()
+  }
+
+  /**
+    * Attempts to mark a single MessageSend as taken, and returns it if it
+    * was successful. Usually it will only return None when there are no messages
+    * to send, but it is also possible when a number of other workers are locking
+    * messages at once.
+    */
+  def lockRecord()(implicit c: Connection): Option[MessageSend.Light] = {
+    // 4 is arbitrary but any number > number of workers is good
+    val select = SQL("SELECT * FROM MESSAGE_SEND WHERE STATE='A' AND ROWNUM <= 4 FOR UPDATE")
+    val update = SQL("UPDATE MESSAGE_SEND SET STATE='T', UPDATED_AT={now} WHERE ID={id} AND STATE='A'")
+
+    val records = select.as(MessageSend.rowParser.*)
+    // We've picked a few rows in case a few have already been taken.
+    // Find the first one that we can successfully transition from A -> T
+    records.find { record =>
+      update.on('id -> record.id, 'now -> DateTime.now).executeUpdate() > 0
+    }
   }
 
 }
--- app/services/messaging/MessagingService.scala
+++ app/services/messaging/MessagingService.scala
@@ -1,15 +1,14 @@
 package services.messaging
 
 import java.sql.Connection
-import java.util.UUID
 import javax.inject.{Inject, Named}
 
 import actors.MessageProcessing.ProcessingResult
 import com.google.inject.ImplementedBy
 import models.Activity
-import org.joda.time.DateTime
 import play.api.db.{Database, NamedDatabase}
 import services.ActivityService
+import services.dao.MessagingDao
 import system.Logging
 import warwick.sso.{UserLookupService, Usercode}
 
@@ -26,31 +25,29 @@ import scala.concurrent.Future
 @ImplementedBy(classOf[MessagingServiceImpl])
 trait MessagingService {
   def send(recipients: Set[Usercode], activity: Activity): Unit
+
   def lockRecord(): Option[MessageSend.Light]
 
   def success(message: MessageSend.Light): Unit
+
   def failure(message: MessageSend.Light): Unit
 
   def processNow(message: MessageSend.Light): Future[ProcessingResult]
 }
 
-class MessagingServiceImpl @Inject() (
+class MessagingServiceImpl @Inject()(
   @NamedDatabase("default") db: Database,
   activities: ActivityService,
   users: UserLookupService,
   @Named("email") emailer: OutputService,
-  @Named("apns") apns: OutputService
+  @Named("apns") apns: OutputService,
+  messagingDao: MessagingDao
 ) extends MessagingService with Logging {
-
-  import anorm._
-  import warwick.anorm.converters.ColumnConversions._
 
   override def send(recipients: Set[Usercode], activity: Activity): Unit = {
     def save(output: Output, user: Usercode)(implicit c: Connection) = {
       if (logger.isDebugEnabled) logger.logger.debug(s"Sending ${output.name} to ${user} about ${activity.id}")
-      SQL"""INSERT INTO MESSAGE_SEND (ID, ACTIVITY_ID, USERCODE, OUTPUT, UPDATED_AT) VALUES
-            ( ${UUID.randomUUID().toString}, ${activity.id}, ${user.string}, ${output.name}, ${new DateTime()} )
-          """.execute()
+      messagingDao.save(activity, user, output)
     }
 
     db.withTransaction { implicit c =>
@@ -76,32 +73,25 @@ class MessagingServiceImpl @Inject() (
     */
   def lockRecord(): Option[MessageSend.Light] = {
     db.withTransaction { implicit connection =>
-      // 4 is arbitrary but any number > number of workers is good
-      val select = SQL("SELECT * FROM MESSAGE_SEND WHERE STATE='A' AND ROWNUM <= 4 FOR UPDATE")
-      val update = SQL("UPDATE MESSAGE_SEND SET STATE='T', UPDATED_AT={now} WHERE ID={id} AND STATE='A'")
-      val now = new DateTime
-
-      val records = select.as(MessageSend.rowParser.*)
-      // We've picked a few rows in case a few have already been taken.
-      // Find the first one that we can successfully transition from A -> T
-      records.find { record =>
-        update.on('id -> record.id, 'now -> now).executeUpdate() > 0
-      }
+      messagingDao.lockRecord()
     }
   }
 
   def success(message: MessageSend.Light): Unit = complete("S", message)
+
   def failure(message: MessageSend.Light): Unit = complete("F", message)
 
   private def complete(newState: String, message: MessageSend.Light): Unit = {
     db.withTransaction { implicit c =>
-      SQL"UPDATE MESSAGE_SEND SET STATE=${newState}, UPDATED_AT=${new DateTime()} WHERE ID=${message.id}".executeUpdate()
+      messagingDao.complete(message.id, newState)
     }
   }
 
   // TODO actually decide whether this user should receive this sort of notification
   def sendEmailFor(user: Usercode, activity: Activity): Boolean = true
+
   def sendSmsFor(user: Usercode, activity: Activity): Boolean = false
+
   def sendMobileFor(user: Usercode, activity: Activity): Boolean = true
 
   override def processNow(message: MessageSend.Light): Future[ProcessingResult] = {
@@ -114,10 +104,10 @@ class MessagingServiceImpl @Inject() (
           case Output.Mobile => apns.send(heavyMessage)
         }
       }.getOrElse {
-        Future.successful(ProcessingResult(success=false, s"User ${message.user} not found"))
+        Future.successful(ProcessingResult(success = false, s"User ${message.user} not found"))
       }
     }.getOrElse {
-      Future.successful(ProcessingResult(success=false, s"Activity ${message.activity} not found"))
+      Future.successful(ProcessingResult(success = false, s"Activity ${message.activity} not found"))
     }
   }
 }
