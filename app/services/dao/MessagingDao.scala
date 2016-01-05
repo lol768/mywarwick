ef565e1ef445e502953243c23c88ddfd9d83fe43
Alec Cursley <a.cursley@warwick.ac.uk>
2016-01-05 15:39:08 +0000

    Log number of items deleted

--- app/services/dao/MessagingDao.scala
+++ app/services/dao/MessagingDao.scala
@@ -3,23 +3,26 @@ package services.dao
 import java.sql.Connection
 
 import anorm.SQL
+import com.google.inject.ImplementedBy
 import org.joda.time.DateTime
 import warwick.anorm.converters.ColumnConversions._
 
+@ImplementedBy(classOf[MessagingDaoImpl])
 trait MessagingDao {
 
-  def deleteOldSuccessfulMessageSends()(implicit c: Connection): Unit
+  def deleteMessagesSuccessfullySentBefore(date: DateTime)(implicit c: Connection): Int
 
 }
 
 class MessagingDaoImpl extends MessagingDao {
 
-  override def deleteOldSuccessfulMessageSends()(implicit c: Connection): Unit =
+  override def deleteMessagesSuccessfullySentBefore(date: DateTime)(implicit c: Connection): Int = {
     SQL("DELETE FROM MESSAGE_SEND WHERE STATE = {state} AND UPDATED_AT < {date}")
       .on(
         'state -> "S",
-        'date -> DateTime.now.minusWeeks(1)
+        'date -> date
       )
-      .execute()
+      .executeUpdate()
+  }
 
 }
--- app/services/job/MessageSendCleanupJob.scala
+++ app/services/job/MessageSendCleanupJob.scala
@@ -1,6 +1,7 @@
 package services.job
 
 import com.google.inject.Inject
+import org.joda.time.DateTime
 import org.quartz.{Job, JobExecutionContext}
 import play.api.db.{Database, NamedDatabase}
 import services.dao.MessagingDao
@@ -13,7 +14,10 @@ class MessageSendCleanupJob @Inject()(
 
   override def execute(jobExecutionContext: JobExecutionContext): Unit =
     db.withConnection { implicit c =>
-      messagingDao.deleteOldSuccessfulMessageSends()
+      val oneWeekAgo = DateTime.now.minusWeeks(1)
+      val itemsDeleted = messagingDao.deleteMessagesSuccessfullySentBefore(oneWeekAgo)
+
+      logger.info(s"Deleted old MESSAGE_SEND row(s): deleted=$itemsDeleted, before=$oneWeekAgo")
     }
 
 }
