647d2e26f747d430fd70b17a2804dc68555ec89e
Nic Duke <n.duke@warwick.ac.uk>
2016-01-06 13:38:35 +0000

    NEWSTART-73 and more changes from code review

--- app/controllers/AppManifestController.scala
+++ app/controllers/AppManifestController.scala
@@ -2,36 +2,39 @@ package controllers
 
 import com.google.inject.Inject
 import play.api.Configuration
-import play.api.libs.json.{JsString, Json}
-import services.SecurityService
+import play.api.cache.Cached
+import play.api.libs.json.Json
+import play.api.mvc.Action
 
 class AppManifestController @Inject()(
-  securityService: SecurityService,
-  configuration: Configuration
+  configuration: Configuration,
+  cached: Cached
 ) extends BaseController {
 
-  import securityService._
+  val gcmSenderId = configuration.getString("start.gcm.id")
+    .getOrElse(throw new IllegalStateException("Missing GCM Sender Id - set start.gcm.id"))
 
-  def getAppManifest = UserAction { implicit request =>
-    Ok(Json.obj(
-      "name" -> JsString("Start.Warwick"),
-      "short_name" -> JsString("Start.Warwick"),
-      "start_url" -> JsString("/"),
-      "display" -> JsString("standalone"),
-      "icons" -> Json.arr(
-        Json.obj(
-          "src" -> JsString("/assets/lib/id7/images/android-chrome-144x144.png"),
-          "sizes" -> JsString("144x144"),
-          "type" -> JsString("image/png")
-        ),
-        Json.obj(
-          "src" -> JsString("/assets/lib/id7/images/android-chrome-192x192.png"),
-          "sizes" -> JsString("192x192"),
-          "type" -> JsString("image/png"
+  def getAppManifest = cached("manifest") {
+    Action {
+      Ok(Json.obj(
+        "name" -> "Start.Warwick",
+        "short_name" -> "Start.Warwick",
+        "start_url" -> "/",
+        "display" -> "standalone",
+        "icons" -> Json.arr(
+          Json.obj(
+            "src" -> "/assets/lib/id7/images/android-chrome-144x144.png",
+            "sizes" -> "144x144",
+            "type" -> "image/png"
+          ),
+          Json.obj(
+            "src" -> "/assets/lib/id7/images/android-chrome-192x192.png",
+            "sizes" -> "192x192",
+            "type" -> "image/png"
           )
-        )
-      ),
-      "gcm_sender_id" -> JsString(configuration.getString("start.gcm.id").getOrElse(throw new IllegalStateException("Missing GCM Sender Id - set start.gcm.id")))
-    ))
+        ),
+        "gcm_sender_id" -> gcmSenderId
+      ))
+    }
   }
 }
