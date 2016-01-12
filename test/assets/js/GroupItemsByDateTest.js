bcd35b64ae8923514103fbad67f236c88282adb9
Nic Duke <n.duke@warwick.ac.uk>
2016-01-12 15:37:30 +0000

    NEWSTART-89 updated mocha tests to accommodate future groupings

--- test/assets/js/GroupItemsByDateTest.js
+++ test/assets/js/GroupItemsByDateTest.js
@@ -14,30 +14,57 @@ describe('grouper', () => {
   let now = moment('2015-12-16T09:00:00Z');
   let groupForItem = (date) => grouper.groupForItem(item(date), now);
 
+  it('groups next week', () => {
+    expect(groupForItem('2015-12-21')).to.equal(0);
+  });
+
+  it('groups later this week', () => {
+    expect(groupForItem('2015-12-18')).to.equal(1);
+  });
+
+  it('groups tomorrow', () => {
+    expect(groupForItem('2015-12-17')).to.equal(2);
+  });
+
   it('groups today', () => {
-    expect(groupForItem('2015-12-16')).to.equal(0);
+    expect(groupForItem('2015-12-16')).to.equal(3);
   });
 
   it('groups yesterday', () => {
-    expect(groupForItem('2015-12-15')).to.equal(1);
+    expect(groupForItem('2015-12-15')).to.equal(4);
   });
 
   it('groups earlier this week', () => {
-    expect(groupForItem('2015-12-14')).to.equal(2);
+    expect(groupForItem('2015-12-14')).to.equal(5);
   });
 
   it('groups last week', () => {
-    expect(groupForItem('2015-12-12')).to.equal(3);
-    expect(groupForItem('2015-12-07')).to.equal(3);
+    expect(groupForItem('2015-12-12')).to.equal(6);
+    expect(groupForItem('2015-12-07')).to.equal(6);
   });
 
   it('considers the most recent past Sunday to be last week', () => {
-    expect(groupForItem('2015-12-13')).to.equal(3);
+    expect(groupForItem('2015-12-13')).to.equal(6);
   });
 
   it('groups earlier than last week', () => {
-    expect(groupForItem('2015-12-06')).to.equal(4);
-    expect(groupForItem('2015-01-01')).to.equal(4);
+    expect(groupForItem('2015-12-06')).to.equal(7);
+    expect(groupForItem('2015-01-01')).to.equal(7);
   });
+
+  /*
+   * testing (Yesterday BUT ALSO Last Week) and (Tomorrow BUT ALSO Next Week)
+   */
+  let groupForItemMon = (date) => grouper.groupForItem(item(date), moment('2015-12-21T09:00:00Z'));
+  let groupForItemSun = (date) => grouper.groupForItem(item(date), moment('2015-12-20T09:00:00Z'));
+
+  it('groups yesterday but also last week', () => {
+    expect(groupForItemMon('2015-12-20')).to.equal(4);
+  });
+
+  it('groups tomorrow but also next week', () => {
+    expect(groupForItemSun('2015-12-21')).to.equal(2);
+  });
+
 
 });
