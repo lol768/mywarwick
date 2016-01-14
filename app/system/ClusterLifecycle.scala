baed5ffc438f6c16ce1046dcf3422c5cabd90e88
Nick Howes <n.howes@warwick.ac.uk>
2016-01-14 15:57:03 +0000

    NEWSTART-128 Only manage cluster when clustering is active.

--- app/system/ClusterLifecycle.scala
+++ app/system/ClusterLifecycle.scala
@@ -6,6 +6,7 @@ import javax.inject.Inject
 import akka.actor.ActorSystem
 import akka.cluster.Cluster
 import akka.pattern.after
+import play.api.Configuration
 import play.api.inject.ApplicationLifecycle
 
 import scala.concurrent.{Future, Promise}
@@ -16,30 +17,36 @@ import scala.util.Try
   * Listens for app shutdown and leaves the cluster cleanly.
   */
 class ClusterLifecycle @Inject() (
+  config: Configuration,
   lifecycle: ApplicationLifecycle,
   akka: ActorSystem
 ) extends Logging {
 
-  val cluster = Cluster(akka)
-  val shutdownWait: FiniteDuration = 30.seconds
+  if (config.getString("akka.actor.provider").contains("akka.cluster.ClusterActorRefProvider")) {
 
-  import akka.dispatcher
+    val cluster = Cluster(akka)
+    val shutdownWait: FiniteDuration = 30.seconds
 
-  lifecycle.addStopHook(() => {
-    logger.info("Leaving cluster because app is shutting down.")
-    val p = Promise[Unit]
-    cluster.leave(cluster.selfAddress)
-    cluster.registerOnMemberRemoved {
-      logger.info("Left cluster cleanly, okay to continue shutdown.")
-      p.complete(Try(Unit))
-    }
+    import akka.dispatcher
 
-    // Give up waiting after a decent amount of time so there's no chance of us
-    // blocking shutdown forever.
-    Future.firstCompletedOf(Seq(
-      p.future,
-      after(shutdownWait, akka.scheduler)(Future.failed(new TimeoutException(s"Waited ${shutdownWait} to leave cluster, timed out")))
-    ))
-  })
+    lifecycle.addStopHook(() => {
+      logger.info("Leaving cluster because app is shutting down.")
+      val p = Promise[Unit]
+      cluster.leave(cluster.selfAddress)
+      cluster.registerOnMemberRemoved {
+        logger.info("Left cluster cleanly, okay to continue shutdown.")
+        p.complete(Try(Unit))
+      }
+
+      // Give up waiting after a decent amount of time so there's no chance of us
+      // blocking shutdown forever.
+      Future.firstCompletedOf(Seq(
+        p.future,
+        after(shutdownWait, akka.scheduler)(Future.failed(new TimeoutException(s"Waited ${shutdownWait} to leave cluster, timed out")))
+      ))
+    })
+
+  }
+
 
 }
