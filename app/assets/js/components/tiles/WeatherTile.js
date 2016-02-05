a8b1d1fc0c7d7ac7230cc2c1011f78647910d9af
Nicholas Duke <nicduke@98-5a-eb-dd-00-5a.csv.warwick.ac.uk>
2016-02-05 09:23:38 +0000

    NEWSTART-163 styling changes and support zoomed content for WeatherTile

--- app/assets/js/components/tiles/WeatherTile.js
+++ app/assets/js/components/tiles/WeatherTile.js
@@ -1,18 +1,47 @@
 import React from 'react';
 
 import Tile from './Tile';
-import Skycon from 'react-skycons';
+import Skycons from 'react-skycons';
+import _ from 'lodash';
+import moment from 'moment';
 
-DEFAULT_ICON_COLOUR = 'white';
+const DEFAULT_ICON_COLOUR = 'white';
+
+function formatIconString(str) {
+  return str.toUpperCase().replace(/-/g, '_');
+}
+
+function formatWeatherTime(d) {
+  const date = moment.unix(d);
+  const now = moment();
+
+  return date.isSame(now, 'hour') ? 'Now' : date.format('ha');
+
+}
 
 export default class WeatherTile extends Tile {
 
+  canZoom() {
+    return true;
+  }
+
   getBody(content) {
+    const itemsToDisplay = this.isZoomed() ? content.items : [content.items[0]];
+
     return (
       <div>
-        <Skycon color={DEFAULT_ICON_COLOUR} icon={content.icon}/>
-        <span className="caption">{content.temp}</span>
-        <span className="caption">{content.caption}</span>
+        {itemsToDisplay.map(item =>
+          (
+            <div key={item.id} className="tile__item">
+              <span className="tile__callout">{`${Math.ceil(item.temp)}°C`}</span>
+              <Skycons className="skycon" color={DEFAULT_ICON_COLOUR}
+                       icon={formatIconString(item.icon)}/>
+              <span className="tile__text">
+                {`${formatWeatherTime(item.time)}: ${item.text}`}
+              </span>
+            </div>
+          )
+        )}
       </div>
     )
   }
