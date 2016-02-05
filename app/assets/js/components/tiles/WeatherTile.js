4d536032b77f02b3bd14f95220aec6f5a0fcdfef
Nicholas Duke <nicduke@98-5a-eb-dd-00-5a.csv.warwick.ac.uk>
2016-02-05 10:05:30 +0000

    NEWSTART-163 all hail eslint

--- app/assets/js/components/tiles/WeatherTile.js
+++ app/assets/js/components/tiles/WeatherTile.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Tile from './Tile';
 import Skycons from 'react-skycons';
-import _ from 'lodash';
 import moment from 'moment';
 
 const DEFAULT_ICON_COLOUR = 'white';
@@ -14,28 +13,24 @@ function formatIconString(str) {
 function formatWeatherTime(d) {
   const date = moment.unix(d);
   const now = moment();
-
   return date.isSame(now, 'hour') ? 'Now' : date.format('ha');
-
 }
 
 export default class WeatherTile extends Tile {
-
   canZoom() {
     return true;
   }
 
   getBody(content) {
     const itemsToDisplay = this.isZoomed() ? content.items : [content.items[0]];
-
     return (
       <div>
-        {itemsToDisplay.map(item =>
-          (
+        {itemsToDisplay.map(item => (
             <div key={item.id} className="tile__item">
               <span className="tile__callout">{`${Math.ceil(item.temp)}°C`}</span>
               <Skycons className="skycon" color={DEFAULT_ICON_COLOUR}
-                       icon={formatIconString(item.icon)}/>
+                icon={formatIconString(item.icon)}
+              />
               <span className="tile__text">
                 {`${formatWeatherTime(item.time)}: ${item.text}`}
               </span>
@@ -43,6 +38,6 @@ export default class WeatherTile extends Tile {
           )
         )}
       </div>
-    )
+    );
   }
 }
