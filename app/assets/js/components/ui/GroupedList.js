ba7fca61b2e68c2ccd0dc8586e1ab2cd7953b761
Alec Cursley <a.cursley@warwick.ac.uk>
2016-02-18 17:05:41 +0000

    Merge pull request #99 in START/app from NEWSTART-186 to develop
    * commit '398c8a43260cc92c7a3bc64458f16e8d2ee9be4e':
      Optimise formatDate slightly
      NEWSTART-186 Group activity items more efficiently

--- app/assets/js/components/ui/GroupedList.js
+++ app/assets/js/components/ui/GroupedList.js
@@ -7,6 +7,20 @@ import ListHeader from './ListHeader';
 
 export default class GroupedList extends ReactComponent {
 
+  getGroupedItems() {
+    const { groupBy, children } = this.props;
+
+    if ('getGroupedItems' in groupBy) {
+      return groupBy.getGroupedItems(children);
+    }
+
+    return _(this.props.children).chain()
+      .groupBy((obj) => this.props.groupBy.groupForItem(obj))
+      .pairs()
+      .sortBy(([group, item]) => group) // eslint-disable-line no-unused-vars
+      .value();
+  }
+
   render() {
     if (this.props.groupBy === undefined) {
       return (
@@ -17,27 +31,22 @@ export default class GroupedList extends ReactComponent {
     }
 
     // Group the child nodes using the object passed to the groupBy property
-    let groups = _(this.props.children)
-    // don't pass directly to groupBy - tramples on your default args.
-      .groupBy((obj) => this.props.groupBy.groupForItem(obj))
-      .pairs()
-      .sortBy(([group, item]) => group); // eslint-disable-line no-unused-vars
+    let groups = this.getGroupedItems();
 
     if (this.props.orderDescending) {
       groups = groups.reverse();
     }
 
     const orderedGroups = groups.map(([group, items]) => (
-        // Title the group with a list header
-        <div key={`group-${group}`} className="list-group">
-          <ListHeader
-            key={`group-header-${group}`}
-            title={this.props.groupBy.titleForGroup(group)}
-          />
-          {items}
-        </div>
-      ))
-      .value();
+      // Title the group with a list header
+      <div key={`group-${group}`} className="list-group">
+        <ListHeader
+          key={`group-header-${group}`}
+          title={this.props.groupBy.titleForGroup(group)}
+        />
+        {items}
+      </div>
+    ));
 
     return (
       <div
