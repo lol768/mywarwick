6a4daca0b186719fea7aded66a4f8c3a2f72376d
Nick Howes <n.howes@warwick.ac.uk>
2016-02-18 17:11:05 +0000

    NEWSTART-194 Remove superclass method.

--- app/assets/js/datapipe/index.js
+++ app/assets/js/datapipe/index.js
@@ -1,18 +1,6 @@
 /* eslint no-unused-vars:0 */
 export default class DataPipe {
 
-  /**
-   * Request from the server any updates that the client may have missed
-   * since last connecting.
-   *
-   * The info should probably be per-tile since some tiles may be more
-   * up to date than others (like if you add a new one or just decide to clear
-   * the data for one).
-   */
-  requestData(info) {
-    throw new TypeError('Must implement requestData');
-  }
-
   send(obj) {
     throw new TypeError('Must implement send');
   }
