9941dc77f62aa1413d4dca5501dda3f829311cf9
Nicholas Duke <nicduke@98-5a-eb-dd-00-5a.csv.warwick.ac.uk>
2016-03-09 13:37:29 +0000

    NEWSTART-184 changes from code review

--- app/assets/js/components/tiles/WeatherTile.js
+++ app/assets/js/components/tiles/WeatherTile.js
@@ -3,13 +3,14 @@ import React from 'react';
 import TileContent from './TileContent';
 import Skycon from '../ui/Skycon';
 import moment from 'moment';
+import { localMomentUnix } from '../../dateFormatter';
 
 function formatIconString(str) {
   return str.toUpperCase().replace(/-/g, '_');
 }
 
 function formatWeatherTime(d) {
-  const date = moment.unix(d);
+  const date = localMomentUnix(d);
   const now = moment();
   return date.isSame(now, 'hour') ? 'Now' : date.format('ha');
 }
@@ -24,49 +25,18 @@ export default class WeatherTile extends TileContent {
     return icon.replace(/.*(clear|rain|snow|sleet|wind|fog|cloudy).*/, '$1');
   }
 
-  mapToTableRow(itemsToDisplay) {
-    return itemsToDisplay.map(item => (
-      <div className="col-xs-2 table__item text--light" key={item.id}>
-        <span className="tile__text">{formatWeatherTime(item.time)}</span>
-        <span className="tile__text">{WeatherTile.oneWordWeather(item.icon)}</span>
-        <span className="tile__text">
-          <i className="fa fa-tint"/> {item.precipProbability * 100}%
-        </span>
-      </div>
-    ));
-  }
-
-  getCallout() {
-    const { content } = this.props;
-    const hour = content.items[0];
-    return (
-      <span className="tile__callout">{`${Math.round(hour.temp)}°`}
-        <small> {hour.text.toLowerCase()}</small>
-      </span>
-    );
-  }
-
-  getCaption() {
-    const { content } = this.props;
-    const nextHour = content.items[1];
-    const mins = moment.unix(nextHour.time).diff(moment(), 'minutes');
-    return (
-      <span className="tile__text--caption">
-            <span className="text--light">Next:</span> {nextHour.text}, in {mins} mins
-            <br/>
-            <span className="text--light">24 hrs:</span> {content.daily.summary}
-      </span>
-    );
-  }
-
   getLargeBody() {
     const { content } = this.props;
     const hour = content.items[0];
     return (
       <div>
-        <div className="col-xs-5">{this.getCallout()}</div>
-        <div className="col-xs-7">{this.getCaption()}</div>
-        {this.mapToTableRow(content.items)}
+        <div className="col-xs-5">
+          <Callout {...content}/>
+        </div>
+        <div className="col-xs-7">
+          <Caption {...content}/>
+        </div>
+        <WeatherTable items={content.items}/>
         <Skycon className="skycon" icon={formatIconString(hour.icon)}/>
       </div>
     );
@@ -76,10 +46,50 @@ export default class WeatherTile extends TileContent {
     const hour = this.props.content.items[0];
     return (
       <div>
-        {this.getCallout()}
-        {this.getCaption()}
+        <Callout {...this.props.content}/>
+        <Caption {...this.props.content}/>
         <Skycon className="skycon" icon={formatIconString(hour.icon)}/>
       </div>
     );
   }
 }
+
+const WeatherTable = (content) =>
+  <div>
+    {content.items.map(item => (
+      <div className="col-xs-2 table__item text--light" key={item.id}>
+        <span className="tile__text">{formatWeatherTime(item.time)}</span>
+        <span className="tile__text">{WeatherTile.oneWordWeather(item.icon)}</span>
+        <span className="tile__text">
+          <i className="fa fa-tint"/> {Math.round(item.precipProbability * 100)}%
+        </span>
+      </div>
+    ))}
+  </div>;
+
+
+const Callout = (content) => {
+  const hour = content.items[0];
+  return (
+    <span className="tile__callout">{Math.round(hour.temp)}°
+      <small> {hour.text.toLowerCase()}</small>
+    </span>
+  );
+};
+
+const Caption = (content) => {
+  const nextHour = content.items[1];
+  const mins = localMomentUnix(nextHour.time).diff(moment(), 'minutes');
+  return (
+    <span className="tile__text--caption">
+      <div className="col-xs-3 text--light">
+        Next:<br/>
+        24 hrs:
+      </div>
+       <div className="col-xs-9">
+         {nextHour.text}, in {mins} mins<br/>
+         {content.daily.summary}
+       </div>
+    </span>
+  );
+};
--- test/assets/js/components/tiles/WeatherTileTest.js
+++ test/assets/js/components/tiles/WeatherTileTest.js
@@ -1,43 +1,58 @@
 import WeatherTile from 'components/tiles/WeatherTile';
 import { renderToStaticMarkup } from 'react-dom/server';
+import { localMomentUnix } from 'dateFormatter';
 
 describe('WeatherTile', () => {
 
-  const props = {
-    "content": {
-      "items": [
-        {"id": 1456316008, "time": 1456316008, "temp": 4.13, "icon": "clear-day", "text": "Clear"},
-        {"id": 1456318800, "time": 1456318800, "temp": 4.71, "icon": "clear-day", "text": "Clear"},
-        {"id": 1456322400, "time": 1456322400, "temp": 5.7, "icon": "clear-day", "text": "Clear"},
-        {"id": 1456326000, "time": 1456326000, "temp": 6.71, "icon": "clear-day", "text": "Clear"},
-        {"id": 1456329600, "time": 1456329600, "temp": 6.39, "icon": "clear-day", "text": "Clear"},
-        {"id": 1456333200, "time": 1456333200, "temp": 5.47, "icon": "clear-day", "text": "Clear"}
-      ],
-      daily: {"summary": "all of today is going to suck!"},
+  const data = {
+      icon: 'clear-day',
+      temp: 4.13,
+      text: 'Clear',
+      precipProbability: 0.55
     },
-    size: 'small',
-  };
+    props = {
+      "content": {
+        "items": [
+          {"id": 1456316008, "time": 1456316008, ...data},
+          {"id": 1456318800, "time": 1456318800, ...data},
+          {"id": 1456322400, "time": 1456322400, ...data},
+          {"id": 1456326000, "time": 1456326000, ...data},
+          {"id": 1456329600, "time": 1456329600, ...data},
+          {"id": 1456333200, "time": 1456333200, ...data},
+        ],
+        daily: {"summary": "all of today is going to suck!"},
+      },
+      size: 'small',
+    };
 
   it('displays a single weather item when size small', () => {
     const html = shallowRender(<WeatherTile { ...props } />);
     html.type.should.equal('div');
 
-    const [callout, caption, skycon] = html.props.children;
+    const callout = shallowRender(html.props.children[0]);
     callout.type.should.equal('span');
     callout.props.className.should.equal('tile__callout');
-    callout.props.children[0].should.equal('4°');
+    callout.props.children[0].should.equal(4); // the ° falls into the next child component
 
+    const caption = shallowRender(html.props.children[1]);
     caption.type.should.equal('span');
     caption.props.className.should.equal('tile__text--caption');
-    caption.props.children[9].should.equal('all of today is going to suck!');
+    caption.props.children[1]
+      .props.children[5].should.equal('all of today is going to suck!');
   });
 
   it('displays large layout when zoomed', () => {
     const html = shallowRender(<WeatherTile zoomed={ true } { ...props } />);
-    const [calloutContainer, captionContainer, items] = html.props.children;
-    calloutContainer.props.children.props.className.should.equal('tile__callout');
-    captionContainer.props.children.props.className.should.equal('tile__text--caption');
-    items.length.should.equal(6);
+    const [calloutContainer, captionContainer, weatherTable] = html.props.children;
+    const callout = shallowRender(calloutContainer.props.children);
+    callout.props.className.should.equal('tile__callout');
+    const caption = shallowRender(captionContainer.props.children);
+    caption.props.className.should.equal('tile__text--caption');
+    const table = shallowRender(weatherTable);
+    table.props.children.length.should.equal(6);
+    table.props.children[0]
+      .props.children[2]
+      .props.children[2].should.equal(55); // precipProbability is rendered
   });
 
   it('formats icon string to single word', () => {
@@ -48,6 +63,12 @@ describe('WeatherTile', () => {
     ];
     const singleWords = icons.map(WeatherTile.oneWordWeather);
     assert.deepEqual(singleWords, ['clear', 'rain', 'cloudy']);
-  })
+  });
+
+  it('renders daylight saving time from localMomentUnix', () => {
+    const unix = 1465560000; // 10 June 2016 12:00
+    const hour = localMomentUnix(unix).format('HH:mm');
+    assert.equal(hour, '13:00'); // assert one hour ahead
+  });
 
 });
