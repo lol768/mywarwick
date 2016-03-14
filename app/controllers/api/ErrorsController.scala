0e88d7f020eaddaea2fa14c1beed6c23e730dc72
Alec Cursley <a.cursley@warwick.ac.uk>
2016-03-14 16:54:00 +0000

    Create JAVASCRIPT_ERROR Logger instance

--- app/controllers/api/ErrorsController.scala
+++ app/controllers/api/ErrorsController.scala
@@ -1,16 +1,19 @@
 package controllers.api
 
 import controllers.BaseController
+import play.api.Logger
 import play.api.libs.json.JsValue
 import play.api.mvc.Action
 
 class ErrorsController extends BaseController {
 
+  override lazy val logger = Logger("JAVASCRIPT_ERROR")
+
   def js = Action { implicit request =>
     request.body.asJson.flatMap(_.validate[Map[String, JsValue]].asOpt).foreach { error =>
       val values = error.map { case (k, v) => s"$k=$v" }.mkString(" ")
 
-      logger.error(s"JAVASCRIPT_ERROR $values")
+      logger.error(values)
     }
     Ok("")
   }
