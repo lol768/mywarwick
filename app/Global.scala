62f6c7b73cd8b087fc54307d6ab5bcf9f385e3b2
Alec Cursley <a.cursley@warwick.ac.uk>
2016-04-06 10:40:34 +0100

    NEWSTART-82 Catch exceptions thrown early in request
    Catch exceptions thrown before the controller action, and mark them in
    the metrics registry as Internal Server Errors.

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,24 @@
-import play.api.GlobalSettings
+import com.codahale.metrics.MetricRegistry.name
+import com.kenshoo.play.metrics.{Metrics, MetricsFilter}
+import play.api.Play.current
+import play.api.mvc.{Action, Handler, RequestHeader}
+import play.api.{GlobalSettings, Play}
 
 object Global extends GlobalSettings {
+
+  override def onRequestReceived(request: RequestHeader): (RequestHeader, Handler) = {
+    try {
+      super.onRequestReceived(request)
+    } catch {
+      case e: Exception =>
+        markInternalServerError()
+        (request, Action.async(onError(request, e)))
+    }
+  }
+
+  private def markInternalServerError() = {
+    val metrics = Play.application.injector.instanceOf[Metrics]
+    metrics.defaultRegistry.meter(name(classOf[MetricsFilter], "500")).mark()
+  }
 
 }
