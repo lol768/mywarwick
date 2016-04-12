a40ab653e8c1dfc9a0f6d7991826559ae2fc340f
Nick Howes <n.howes@warwick.ac.uk>
2016-04-12 17:43:34 +0100

    Force serviceworker update

--- app/assets/js/push-worker.js
+++ app/assets/js/push-worker.js
@@ -1,5 +1,9 @@
 import log from 'loglevel';
 
+// This is mostly to force a serviceworker update if you make a change
+// that doesn't change any assets. Increment updateid.
+log.debug('push worker (updateid:1)');
+
 // Set the callback for the install step
 self.addEventListener('install', () => {
   // Perform install steps
