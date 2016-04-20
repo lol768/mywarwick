280c3853a3bdb2d67972e0e2698da5a841aba072
Nicholas Duke <nicduke@98-5a-eb-dd-00-5a.csv.warwick.ac.uk>
2016-04-20 12:17:50 +0100

    NEWSTART-273 extend ListTile into CourseworkTile subclass, and define small body rendition. add tests

---
+++ app/assets/js/components/tiles/CourseworkTile.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ListTile from './ListTile';
+import formatDate, { localMoment } from '../../dateFormatter';
+import _ from 'lodash';
+
+function hyperlinkText(text, href) {
+  return href ?
+    <a href={ href } target="_blank" ref="a">{ text }</a>
+    : text;
+}
+
+export default class CourseworkTile extends ListTile {
+
+  getSmallBody() {
+    function numAssignmentsDue(assignments) {
+      const items = _.takeWhile(assignments, (a) =>
+        localMoment(a.date).isBefore(localMoment().add(30, 'd'), 'day'));
+      return items.length;
+    }
+
+    const { content } = this.props;
+    const { text, date, href } = content.items[0];
+
+    const nextAssignment = (
+      <span className="tile__text">
+        Next: {hyperlinkText(text, href)}, due {formatDate(date)}
+      </span>
+    );
+
+    const numAssignments = numAssignmentsDue(content.items);
+
+    const callout = (
+      <span className="tile__callout">
+        { numAssignments }
+        <small> assignment{numAssignments === 1 ? null : 's'} in next month</small>
+      </span>
+    );
+
+    if (numAssignments === 0) {
+      return (
+        <div className="tile__item">
+          { callout }
+        </div>
+      );
+    }
+
+    return (
+      <div className="tile__item">
+        { callout }
+        { nextAssignment }
+      </div>
+    );
+  }
+}
---
+++ test/assets/js/components/tiles/CourseworkTileTest.js
@@ -0,0 +1,40 @@
+import CourseworkTile from 'components/tiles/CourseworkTile';
+
+describe('CourseworkTile', () => {
+
+  const props = {
+    content: {
+      items: [
+        {
+          date: "2016-05-12T00:00:00+01:00",
+          href: "n/a",
+          id: "ed",
+          text: "Parachuting (SP747)",
+          title: "Parachuting",
+        },
+        {
+          date: "2016-05-16T00:00:00+01:00",
+          href: "n/a",
+          id: "edd",
+          text: "Tactical Espionage Action (SN4k3)",
+          title: "Tactical Espionage Action",
+        },
+        {
+          date: "2016-05-22T00:00:00+01:00",
+          href: "n/a",
+          id: "eddy",
+          text: "Health and Safety (IN102)",
+          title: "Health and Safety",
+        },
+      ]
+    }
+  };
+
+  it('renders small tile with assignment count for next month', () => {
+    const html = renderAtMoment(<CourseworkTile {...props} size="small" />, new Date(2016, 3, 20));
+    findChild(html, [0, 0]).should.equal(2); // third item > 30 days in future
+    findChild(html, [1, 1]).type.should.equal('a');
+    findChild(html, [1, 1]).props.children.should.equal('Parachuting (SP747)');
+    findChild(html, [1, 3]).should.equal('Thu 12 May, 0:00');
+  })
+});
