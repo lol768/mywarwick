f668f32409a043ecd6f1e6ecb1e4379fef7b442c
Nick Howes <n.howes@warwick.ac.uk>
2016-05-03 16:26:59 +0100

    Export DDL from test.

--- test/integration/ApplicationTest.scala
+++ test/integration/ApplicationTest.scala
@@ -1,15 +1,20 @@
 package integration
 
+import java.io.PrintWriter
+
 import controllers.HomeController
 import helpers.OneStartAppPerSuite
 import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
 import play.api.Configuration
 import play.api.Environment
+import play.api.db.Database
 import play.api.inject._
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers._
 import services.{ActivityService, TileContentService}
 import warwick.sso._
+import anorm._
+import anorm.SqlParser._
 
 /**
   * Eventually we'll have a suite of integration tests, that actually
@@ -29,8 +34,24 @@ class ApplicationTest extends PlaySpec with OneStartAppPerSuite {
       // work with a local-only actor system. Should be override PubSub for tests?
       //app.injector.instanceOf[ActivityService]
 
+      // Output the test schema to a file, just so we can see what the tables look like
+      // after all migrations have been applied.
+      val db = app.injector.instanceOf[Database]
+      exportH2Schema(db, "target/ddl.sql")
+
     }
 
+  }
+
+  def exportH2Schema(db: Database, path: String): Unit = {
+    db.withConnection { implicit conn =>
+      val writer = new PrintWriter(path)
+      try {
+        SQL("SCRIPT NODATA").as(str(1).*).foreach(writer.println)
+      } finally {
+        writer.close()
+      }
+    }
   }
 
 }
