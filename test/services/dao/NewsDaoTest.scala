b0f4b57758ae538438d0faa912425ada5e3e4ca9
Nick Howes <n.howes@warwick.ac.uk>
2016-05-17 20:21:32 +0100

    NEWSTART-302 Fix tests.
    Forgot to update them like a fool

--- test/services/dao/NewsDaoTest.scala
+++ test/services/dao/NewsDaoTest.scala
@@ -4,10 +4,12 @@ import helpers.OneStartAppPerSuite
 import models.news.NewsItemSave
 import org.joda.time.DateTime
 import org.scalatestplus.play.PlaySpec
+import services.NewsService
 import warwick.sso.Usercode
 
 class NewsDaoTest extends PlaySpec with OneStartAppPerSuite {
   val newsDao = get[NewsDao]
+  val newsService = get[NewsService]
 
   val ana = Usercode("cusana")
   val bob = Usercode("cusbob")
@@ -42,9 +44,9 @@ class NewsDaoTest extends PlaySpec with OneStartAppPerSuite {
     }
 
     "return only published news for user" in transaction { implicit c =>
-      newsDao.save(londonsBurning, Seq(ana, jim))
-      newsDao.save(brumPanic, Seq(ana, eli))
-      newsDao.save(futureNews, Seq(ana, bob, eli, jim))
+      newsService.save(londonsBurning, Seq(ana, jim))
+      newsService.save(brumPanic, Seq(ana, eli))
+      newsService.save(futureNews, Seq(ana, bob, eli, jim))
 
       val jimNews = newsDao.latestNews(jim)
       jimNews.map(_.title) must be(Seq(londonsBurning.title))
@@ -54,8 +56,8 @@ class NewsDaoTest extends PlaySpec with OneStartAppPerSuite {
     }
 
     "limit results to requested amount" in transaction { implicit c =>
-      newsDao.save(londonsBurning, Seq(ana))
-      newsDao.save(brumPanic, Seq(ana))
+      newsService.save(londonsBurning, Seq(ana))
+      newsService.save(brumPanic, Seq(ana))
       newsDao.latestNews(ana, limit = 2) must have length(2)
       newsDao.latestNews(ana, limit = 1) must have length(1)
     }
