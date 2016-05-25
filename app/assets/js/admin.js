613604aff87a3b8178b55b720fb03ab370e9b72c
Alec Cursley <a.cursley@warwick.ac.uk>
2016-05-25 14:15:28 +0100

    Remove unused imports (lint)

--- app/assets/js/admin.js
+++ app/assets/js/admin.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
-
 /*
   Attempt to register service worker - we don't do notifications or offline but it's nice to keep it
   up to date.
