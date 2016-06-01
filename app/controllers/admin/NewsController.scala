1d97b64ba3dcd3d8d8c57e9f47bdcfa828afecac
Nick Howes <n.howes@warwick.ac.uk>
2016-06-01 12:30:07 +0100

    NEWSTART-329 Pull out Audience binder.

--- app/controllers/admin/NewsController.scala
+++ app/controllers/admin/NewsController.scala
@@ -1,22 +1,18 @@
 package controllers.admin
 
-import javax.inject.Inject
+import javax.inject.{Inject, Singleton}
 
 import controllers.BaseController
 import models.DateFormats
-import models.news.Audience.{DepartmentAudience, DepartmentSubset}
 import models.news.{Audience, Link, NewsItemSave}
-import org.joda.time.{DateTime, LocalDate, LocalDateTime}
+import org.joda.time.LocalDateTime
 import play.api.data.Forms._
 import play.api.data._
-import play.api.data.validation._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import services.dao.DepartmentInfoDao
 import services.{AudienceService, NewsService, SecurityService}
 import system.{Roles, TimeZones}
-import uk.ac.warwick.util.core.StringUtils
 import uk.ac.warwick.util.web.Uri
-import warwick.sso.Usercode
 
 import scala.concurrent.Future
 
@@ -24,7 +20,7 @@ case class PublishNewsData(
   item: NewsItemData,
   audience: Seq[String],
   department: Option[String]
-)
+) extends AudienceFormData
 
 case class NewsItemData(
   title: String,
@@ -45,12 +41,14 @@ case class NewsItemData(
   )
 }
 
+@Singleton
 class NewsController @Inject() (
   security: SecurityService,
   val messagesApi: MessagesApi,
   news: NewsService,
   departments: DepartmentInfoDao,
-  audiences: AudienceService
+  audiences: AudienceService,
+  audienceBinder: AudienceBinder
 ) extends BaseController with I18nSupport {
 
   import Roles._
@@ -85,64 +83,6 @@ class NewsController @Inject() (
     )(PublishNewsData.apply)(PublishNewsData.unapply)
   )
 
-  /**
-    * Attempts to convert the request parameters into an Audience object.
-    * If there are any problems it returns a Seq of FormErrors.
-    *
-    * Return type is a future because it depends on the list of departments.
-    *
-    * TODO move this out of the controller, for use with notifications and the API.
-    */
-  def parseAudience(data: PublishNewsData): Future[Either[Seq[FormError], Audience]] = for {
-    allDepts <- departments.allDepartments
-  } yield {
-    var errors = Seq[FormError]()
-
-    val groupedComponents = data.audience.groupBy(_.startsWith("Dept:"))
-
-    // Bits of audience not related to a department.
-    val globalComponents = groupedComponents.getOrElse(false, Nil).flatMap {
-      case Audience.ComponentParameter(component) => Some(component)
-      case unrecognised =>
-        errors :+= FormError("audience", "error.audience.invalid", unrecognised)
-        None
-    }
-
-    val deptComponentValues = groupedComponents.getOrElse(true, Nil)
-      .map(_.replaceFirst("^Dept:",""))
-      .flatMap {
-        case Audience.DepartmentSubset(subset) => Some(subset)
-        case unrecognised =>
-          errors :+= FormError("audience", "error.audience.invalid", s"Dept:$unrecognised")
-          None
-      }
-
-    val departmentParam = data.department.filter(StringUtils.hasText).map(_.trim)
-    val department = departmentParam.flatMap { code =>
-      allDepts.find(_.code == code)
-    }
-    val deptComponent = department match {
-      case Some(d) if deptComponentValues.nonEmpty => Some(DepartmentAudience(d.code, deptComponentValues))
-      case Some(d) =>
-        errors :+= FormError("department", "error.audience.noDepartmentSubsets")
-        None
-      case None if data.department.isDefined =>
-        errors :+= FormError("department", "error.department.invalid")
-        None
-      case None => None // No department audience - no problem
-    }
-
-    if (department.isEmpty && globalComponents.isEmpty) {
-      errors :+= FormError("audience", "error.audience.empty")
-    }
-
-    if (errors.nonEmpty) {
-      Left(errors)
-    } else {
-      Right(Audience(globalComponents ++ deptComponent))
-    }
-  }
-
   def list = RequiredActualUserRoleAction(Sysadmin) {
     Ok(views.html.admin.news.list(news.allNews(limit = 100)))
   }
@@ -162,7 +102,7 @@ class NewsController @Inject() (
       val bound = publishNewsForm.bindFromRequest
       bound.fold(
         errorForm => Future.successful(Ok(views.html.admin.news.createForm(errorForm, dopts))),
-        data => parseAudience(data).map {
+        data => audienceBinder.bindAudience(data).map {
           case Left(errors) =>
             val errorForm = addErrors(bound, errors)
             Ok(views.html.admin.news.createForm(errorForm, dopts))
