ed114c3f1dc811d69874544c7ae05ee03878886c
Nic Duke <n.duke@warwick.ac.uk>
2016-06-29 10:39:50 +0100

    NEWSTART-358 simplify rounding down of device width, using lodash

--- app/assets/js/state/device.js
+++ app/assets/js/state/device.js
@@ -1,14 +1,8 @@
+import _ from 'lodash';
+
 function calcDeviceWidth() {
   const w = window.innerWidth * window.devicePixelRatio;
-  if (w <= 320) return 320; // rubbish phone, mate
-  else if (w <= 640) return 640; // iPhone 4/5 portrait
-  else if (w <= 750) return 750; // iPhone 6 portrait
-  else if (w <= 960) return 960; // iPhone 4 landscape
-  else if (w <= 1080) return 1080; // iPhone 6p portrait
-  else if (w <= 1136) return 1136; // iPhone 5 landscape
-  else if (w <= 1334) return 1334; // iPhone 6 landscape
-  else if (w <= 2208) return 2208; // iPhone 6p landscape
-  return w;
+  return _.find([320, 640, 750, 960, 1080, 1136, 1334, 2208], width => width >= w);
 }
 
 const initialState = {
