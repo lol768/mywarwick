ec5afd053c3a4a550c991d3b74f9dd55d7ccdf7b
Nic Duke <n.duke@warwick.ac.uk>
2016-07-05 14:37:39 +0100

    NEWSTART-370 remove unused method

--- app/services/NewsCategoryService.scala
+++ app/services/NewsCategoryService.scala
@@ -6,13 +6,10 @@ import play.api.db.Database
 import play.db.NamedDatabase
 import services.dao.NewsCategoryDao
 
-
 @ImplementedBy(classOf[NewsCategoryServiceImpl])
 trait NewsCategoryService {
 
   def all(): Seq[NewsCategory]
-
-  def saveNewsCategories(newsItemId: String, categoryIds: Seq[String]): Unit
 
   def getNewsCategories(newsItemId: String): Seq[NewsCategory]
 
@@ -26,9 +23,6 @@ class NewsCategoryServiceImpl @Inject()(
 ) extends NewsCategoryService {
 
   override def all() = db.withConnection(implicit c => dao.all())
-
-  override def saveNewsCategories(newsItemId: String, categoryIds: Seq[String]) =
-    db.withConnection(implicit c => dao.saveNewsCategories(newsItemId, categoryIds))
 
   override def updateNewsCategories(newsItemId: String, categoryIds: Seq[String]) =
     db.withConnection { implicit c =>
