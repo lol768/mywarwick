c9d78671ffe0b5ef64fcda598606ea583b4ec272
Alec Cursley <a.cursley@warwick.ac.uk>
2016-07-12 16:14:14 +0100

    NEWSTART-362 Two-column layout for news category front-end

--- app/assets/js/components/ui/Link.js
+++ app/assets/js/components/ui/Link.js
@@ -9,22 +9,20 @@ export default class Link extends ReactComponent {
 
   render() {
     return (
-      <li>
-        <a
-          className="link-block__item" href={this.props.href} target="_blank"
-          onClick={this.onClick}
-        >
-          {
-            this.props.subtitle ?
-              <span>
-            <span className="link-block__item__title">{this.props.children}</span>
-            <small className="link-block__item__subtitle">{this.props.subtitle}</small>
-        </span>
-              :
-              this.props.children
-          }
-        </a>
-      </li>
+      <a
+        className="link-block__item" href={this.props.href} target="_blank"
+        onClick={this.onClick}
+      >
+        {
+          this.props.subtitle ?
+            <span>
+          <span className="link-block__item__title">{this.props.children}</span>
+          <small className="link-block__item__subtitle">{this.props.subtitle}</small>
+      </span>
+            :
+            this.props.children
+        }
+      </a>
     );
   }
 
