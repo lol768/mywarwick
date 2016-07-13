08e3eb665281662b799de6acf4fd4b85cbfb0d5f
Alec Cursley <a.cursley@warwick.ac.uk>
2016-07-13 16:44:09 +0100

    NEWSTART-386 Fix editing news items
    Decouple the date/time picker from what gets sent back to the server, as
    this was causing validation problems.
    
    Correct the action set on the update form.

--- app/assets/js/admin/news.js
+++ app/assets/js/admin/news.js
@@ -1,27 +1,5 @@
 import $ from 'jquery';
-import 'eonasdan-bootstrap-datetimepicker';
-
-const icons = {
-  time: 'fa fa-clock-o',
-  date: 'fa fa-calendar',
-  up: 'fa fa-chevron-up',
-  down: 'fa fa-chevron-down',
-  previous: 'fa fa-chevron-left',
-  next: 'fa fa-chevron-right',
-  today: 'fa fa-crosshairs',
-  clear: 'fa fa-trash-o',
-  close: 'fa fa-times',
-};
-
-$('.datetimepicker')
-  .attr('type', 'text')
-  .datetimepicker({
-    format: 'YYYY-MM-DDTHH:mm',
-    icons,
-    sideBySide: true,
-    allowInputToggle: true,
-    stepping: 5,
-  });
+import './datetimepicker';
 
 $('input[name="item.publishDateSet"]').on('change', function onChange() {
   const showDateField = $(this).filter(':checked').val() === 'true';
