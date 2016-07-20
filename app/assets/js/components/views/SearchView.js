39f724b6513a7fff0fb2b139b3a24b1fd1d2cfc0
Alec Cursley <a.cursley@warwick.ac.uk>
2016-07-20 10:25:31 +0100

    NEWSTART-395 Use <WarwickSearch /> component to embed Search

--- app/assets/js/components/views/SearchView.js
+++ app/assets/js/components/views/SearchView.js
@@ -1,27 +1,7 @@
-import React, { PropTypes } from 'react';
 import $ from 'jquery';
 
-const defaultRootUrl = $('#app-container').attr('data-search-root-url');
+import WarwickSearch from 'warwick-search-frontend';
 
-const SearchView = (props) => {
-  const rootUrl = props.rootUrl || defaultRootUrl;
+const searchRootUrl = $('#app-container').attr('data-search-root-url');
 
-  let src = `${rootUrl}/embed`;
-
-  if (props.query) {
-    src += `?q=${props.query}`;
-  }
-
-  return (
-    <div className="iframe-embed">
-      <iframe src={ src }></iframe>
-    </div>
-  );
-};
-
-SearchView.propTypes = {
-  query: PropTypes.string,
-  rootUrl: PropTypes.string,
-};
-
-export default SearchView;
+export default WarwickSearch(searchRootUrl);
