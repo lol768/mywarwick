efbae1e5038eb9a56791c9fb39d7b4f639f66cc3
Alec Cursley <a.cursley@warwick.ac.uk>
2016-08-02 11:18:39 +0100

    Some functional testing
    Added a browser definition to allow running tests in Chrome's device
    emulation mode by including the browser name `chrome-mobile`.
    
    Expanded on the tests in BasicFuncTest to click around the UI a bit.
    
    Added a SearchFuncTest as a sanity check for the Start/Search
    integration.

---
+++ functional-test/SearchFuncTest.scala
@@ -0,0 +1,53 @@
+import helpers.FuncTestBase
+import org.scalatest.BeforeAndAfter
+import org.scalatestplus.play.BrowserInfo
+
+class SearchFuncTest extends FuncTestBase with BeforeAndAfter {
+
+  override def sharedTests(info: BrowserInfo): Unit = {
+    val searchResults = className("search-result")
+
+    "Someone lost" should {
+
+      s"be able to search ${info.name}" in withScreenshot {
+        resizeWindow(iphone5Size)
+
+        go to search
+
+        eventually {
+          val activeTabBarItem = find(className("tab-bar-item--active")).get
+          activeTabBarItem.text shouldBe "Search"
+
+          find(id("q-input")) shouldBe defined
+          searchField("q-input").attribute("placeholder") should contain("Search Warwick")
+
+          val pageText = find(cssSelector("main")).get.text
+          pageText should include("Quick links")
+          pageText should include("We thought this might be useful to you")
+        }
+
+        searchField("q-input").value = "elab"
+        submit()
+
+        eventually {
+          val firstSearchResult = find(searchResults).get
+          firstSearchResult.text should include("E-lab home page")
+
+          val pageText = find(cssSelector("main")).get.text
+          pageText should include("Website search results")
+        }
+
+        val searchResultCount = findAll(searchResults).length
+
+        // Scroll to bottom of page so more results link is not occluded by the tab bar
+        // when Selenium tries to click it
+        executeScript("window.scrollTo(0, 10000)")
+
+        click on linkText("More website search results")
+
+        eventually(findAll(searchResults).length should be > searchResultCount)
+      }
+
+    }
+  }
+}
---
+++ functional-test/helpers/ChromeMobileInfo.scala
@@ -0,0 +1,20 @@
+package helpers
+
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.scalatestplus.play.BrowserInfo
+
+import scala.collection.JavaConverters._
+
+case object ChromeMobileInfo extends BrowserInfo("[Chrome Mobile]", "org.scalatest.tags.ChromeBrowser") {
+
+  override def createWebDriver() = {
+    val capabilities = DesiredCapabilities.chrome()
+    capabilities.setCapability(ChromeOptions.CAPABILITY, Map(
+      "mobileEmulation" -> Map("deviceName" -> "Google Nexus 5").asJava
+    ).asJava)
+
+    new ChromeDriver(capabilities)
+  }
+
+}
