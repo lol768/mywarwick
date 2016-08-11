18612e2eac3ab8acf0131b04632bcd13b0cf85f1
Nic Duke <n.duke@warwick.ac.uk>
2016-08-11 12:41:56 +0100

    NEWSTART-371 split Notifications publish form

--- app/controllers/admin/NotificationsController.scala
+++ app/controllers/admin/NotificationsController.scala
@@ -6,9 +6,11 @@ import controllers.BaseController
 import models.{Activity, ActivityIcon, ActivityResponse}
 import models.news.{Audience, NotificationData}
 import models.publishing.Ability.{CreateNotifications, ViewNotifications}
+import models.publishing.Publisher
 import play.api.data.Forms._
 import play.api.data._
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.Result
 import services._
 import services.dao.DepartmentInfoDao
 import system.Validation
@@ -59,10 +61,11 @@ class NotificationsController @Inject()(
   }
 
   def createForm(publisherId: String) = PublisherAction(publisherId, CreateNotifications) { implicit request =>
-    Ok(views.createForm(request.publisher, publishNotificationForm, departmentOptions, providerOptions, permissionScope))
+    Ok(renderCreateForm(request.publisher, publishNotificationForm))
   }
 
   def create(publisherId: String) = PublisherAction(publisherId, CreateNotifications).async { implicit request =>
+    val validateOnly = request.body.asFormUrlEncoded.get.contains("validateOnly")
     val form = publishNotificationForm.bindFromRequest
 
     form.fold(
@@ -73,6 +76,8 @@ class NotificationsController @Inject()(
             Ok(views.createForm(request.publisher, addFormErrors(form, errors), departmentOptions, providerOptions, permissionScope))
           case Right(Audience.Public) =>
             Ok(views.createForm(request.publisher, form.withError("audience", "Notifications cannot be public"), departmentOptions, providerOptions, permissionScope))
+          case Right(audience) if validateOnly =>
+            Ok(renderCreateForm(request.publisher, publishNotificationForm.fill(publish)))
           case Right(audience) =>
             val notification = publish.item.toSave(request.context.user.get.usercode, publisherId)
 
@@ -82,6 +87,16 @@ class NotificationsController @Inject()(
             Redirect(routes.NotificationsController.list(publisherId)).flashing("result" -> "Notification created")
         }
       }
+    )
+  }
+
+  def renderCreateForm(publisher: Publisher, form: Form[PublishNotificationData])(implicit request: PublisherRequest[_]) = {
+    views.createForm(
+      publisher = publisher,
+      form = form,
+      departmentOptions = departmentOptions,
+      providerOptions = providerOptions,
+      permissionScope = permissionScope
     )
   }
 
