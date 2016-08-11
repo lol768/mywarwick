8bba7c116d39c76e0e7c16e2062cd6e15e51bb91
Nic Duke <n.duke@warwick.ac.uk>
2016-08-11 17:37:22 +0100

    NEWSTART-371 scope script to split-form

--- app/assets/js/admin/form-pagination.js
+++ app/assets/js/admin/form-pagination.js
@@ -3,83 +3,89 @@ import 'jquery-form/jquery.form.js';
 
 const SPLIT_FORM = 'form.split-form';
 
-function getHashNum() {
-  return parseInt(location.hash.substring(1), 10);
-}
+$(SPLIT_FORM).each((i, form) => {
+  const $form = $(form);
 
-let currentPage = getHashNum() || 0;
-
-function showSection(num) {
-  history.pushState({ pageNum: num }, location.href);
-  currentPage = num;
-  const section = `section:eq(${num})`;
-  $('.active').removeClass('active').hide();
-  $(section).show().addClass('active').fadeIn();
-}
-
-$('section:not(:last)').append('<button class="btn btn-primary next pull-right" >Next</button>');
-$('section:not(:first)').append('<button class="btn btn-default back pull-left" >Back</button>');
-
-showSection(currentPage);
-
-function validate() {
-  return new Promise((resolve, reject) =>
-    $(SPLIT_FORM).ajaxSubmit({
-      success: resolve,
-      error: reject,
-      data: { validateOnly: true },
-      resetForm: false,
-    })
-  );
-}
-
-function replaceErrors(errs) {
-  $.map(errs, err => $(`#${err.id}`).replaceWith(err));
-}
-
-function checkFormErrors(html) {
-  return new Promise((res, rej) => {
-    const errors = $(html).find(`section:eq(${currentPage}) .has-error`);
-    if (errors.length > 0) {
-      rej(errors);
-    } else {
-      res();
-    }
-  });
-}
-
-$('button.next').on('click', e => {
-  e.preventDefault();
-  // history.pushState({ pageNum: currentPage }, location.href);
-  validate()
-    .then(html => checkFormErrors(html)
-      .then(() => {
-        $(`section:eq(${currentPage}) > *`).removeClass('has-error');
-        return showSection(currentPage + 1);
-      })
-      .catch(replaceErrors)
-    ).catch(() => {}); // TODO: log ting?
-});
-
-window.onpopstate = e => {
-  if (e.state !== null) {
-    showSection(e.state.pageNum);
+  function getHashNum() {
+    return parseInt(location.hash.substring(1), 10);
   }
-};
 
-$('button.back').on('click', e => {
-  e.preventDefault();
-  showSection(currentPage - 1);
-});
+  let currentPage = getHashNum() || 0;
 
+  function showSection(num) {
+    history.pushState({ pageNum: num }, location.href);
+    currentPage = num;
+    const $section = $form.find(`section:eq(${num})`);
+    $form.find('section.active').removeClass('active').hide();
+    $section.show().addClass('active').fadeIn();
+  }
 
-$(SPLIT_FORM).on('submit', () => {
-  $(this).off();
-  validate()
-    .then(html =>
-      checkFormErrors(html)
-        .then(() => $(SPLIT_FORM).off().submit())
+  $form.find('section:not(:last)')
+    .append('<button class="btn btn-primary next pull-right" >Next</button>');
+  $form.find('section:not(:first)')
+    .append('<button class="btn btn-default back pull-left" >Back</button>');
+
+  showSection(currentPage);
+
+  function validate() {
+    return new Promise((resolve, reject) =>
+      $form.ajaxSubmit({
+        success: resolve,
+        error: reject,
+        data: { validateOnly: true },
+        resetForm: false,
+      })
+    );
+  }
+
+  function replaceErrors(errs) {
+    $.map(errs, err => $(`#${err.id}`).replaceWith(err));
+  }
+
+  function checkFormErrors(html) {
+    return new Promise((res, rej) => {
+      const errors = $(html).find(`section:eq(${currentPage}) .has-error`);
+      if (errors.length > 0) {
+        rej(errors);
+      } else {
+        res();
+      }
+    });
+  }
+
+  $('button.next').on('click', e => {
+    e.preventDefault();
+    // history.pushState({ pageNum: currentPage }, location.href);
+    validate()
+      .then(html => checkFormErrors(html)
+        .then(() => {
+          $form.find(`section:eq(${currentPage}) > *`).removeClass('has-error');
+          return showSection(currentPage + 1);
+        })
         .catch(replaceErrors)
-    ).catch(() => {}); // TODO: log ting?
-  return false;
+      ).catch(() => {}); // TODO: log ting?
+  });
+
+  window.onpopstate = e => {
+    if (e.state !== null) {
+      showSection(e.state.pageNum);
+    }
+  };
+
+  $('button.back').on('click', e => {
+    e.preventDefault();
+    showSection(currentPage - 1);
+  });
+
+
+  $form.on('submit', () => {
+    $(this).off();
+    validate()
+      .then(html =>
+        checkFormErrors(html)
+          .then(() => $form.off().submit())
+          .catch(replaceErrors)
+      ).catch(() => {}); // TODO: log ting?
+    return false;
+  });
 });
