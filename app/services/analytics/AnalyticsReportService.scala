5874c8bdd1d174d17c0a311c260c05505a624f95
kai@shylock <k.lan.1@warwick.ac.uk>
2016-09-29 16:13:00 +0100

    NEWSTART-454 extracted the bits for sending GA report request and receive GA report reponse from AnalyticsReportService to AnalyticsTransport.

--- app/services/analytics/AnalyticsReportService.scala
+++ app/services/analytics/AnalyticsReportService.scala
@@ -1,11 +1,6 @@
 package services.analytics
 
-import java.io.File
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.analyticsreporting.v4.model._
-import com.google.api.services.analyticsreporting.v4.{AnalyticsReporting, AnalyticsReportingScopes}
 import com.google.inject.{ImplementedBy, Inject}
 import play.api.Configuration
 
@@ -40,42 +35,21 @@ trait AnalyticsReportService {
 }
 
 class AnalyticsReportServiceImpl @Inject()(
-  config: Configuration
+  config: Configuration,
+  analyticsTransport: AnalyticsTransport
 ) extends AnalyticsReportService {
 
   // DateRange object accepts "nDaysAgo" string for start and end dates
   private def daysAgo(num: Int) = s"${num}DaysAgo"
 
-  private val APPLICATION_NAME = "Start"
-  private val JSON_FACTORY = new JacksonFactory()
-
-  private val KEY_FILE_LOCATION = config.getString("start.analytics.key_path")
-    .getOrElse(throw new IllegalStateException("Missing Google Analytics key file path - set start.analytics.key_path"))
-  private val SERVICE_ACCOUNT_EMAIL = config.getString("start.analytics.account.email")
-    .getOrElse(throw new IllegalStateException("Missing Google Analytics service account id - set start.analytics.account.email"))
-
   //view id for guest
-  private val VIEW_ID_GUEST = config.getString("start.analytics.view-id.guests")
-    .getOrElse(throw new IllegalStateException("Missing Google Analytics view id - set start.analytics.view_id_guest"))
+  val VIEW_ID_GUEST = config.getString("start.analytics.view-id.guests")
+    .getOrElse(throw new IllegalStateException("Missing Google Analytics view id - set start.analytics.view-id.guests"))
 
   //view id for logged in user
-  private val VIEW_ID_LOGGED_IN_USER = config.getString("start.analytics.view-id.users")
-    .getOrElse(throw new IllegalStateException("Missing Google Analytics view id - set start.analytics.view_id_logged_in_user"))
+  val VIEW_ID_LOGGED_IN_USER = config.getString("start.analytics.view-id.users")
+    .getOrElse(throw new IllegalStateException("Missing Google Analytics view id - set start.analytics.view-id.users"))
 
-
-  private val analytics: AnalyticsReporting = {
-    val httpTransport = GoogleNetHttpTransport.newTrustedTransport()
-    val credential = new GoogleCredential.Builder()
-      .setTransport(httpTransport)
-      .setJsonFactory(JSON_FACTORY)
-      .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
-      .setServiceAccountPrivateKeyFromP12File(new File(KEY_FILE_LOCATION))
-      .setServiceAccountScopes(AnalyticsReportingScopes.all())
-      .build()
-
-    new AnalyticsReporting.Builder(httpTransport, JSON_FACTORY, credential)
-      .setApplicationName(APPLICATION_NAME).build()
-  }
 
   private def ga(str: String) = s"ga:$str"
 
@@ -102,31 +76,18 @@ class AnalyticsReportServiceImpl @Inject()(
 
     val dimensionsSet = dimensions.map(str => new Dimension().setName(ga(str)))
 
-    val filterClaus = new DimensionFilterClause() // ho, ho, ho, merry filtering!
+    val filterClause = new DimensionFilterClause()
       .setFilters(filters.asJava)
 
     def newReportRequest = new ReportRequest()
       .setDateRanges(java.util.Arrays.asList(dateRange))
       .setMetrics(metricsSet.asJava)
       .setDimensions(dimensionsSet.asJava)
-      .setDimensionFilterClauses(java.util.Arrays.asList(filterClaus))
-
-
-    def resultToRows(response: GetReportsResponse) = response.getReports.asScala.flatMap { report =>
-      val rows = Option(report.getData.getRows)
-      rows.map(_.asScala).getOrElse(Nil).map { row =>
-        Row(
-          row.getDimensions.get(0).split("/")(2), // get newsId substring from pagePath
-          row.getMetrics.get(0).getValues.get(0).toInt
-        )
-      }
-    }
+      .setDimensionFilterClauses(java.util.Arrays.asList(filterClause))
 
     /*
-    google does not allow getting reports for multiple requests, so we have to get these reports
-    one by one
-
-    the process is like request -> result -> report
+    google does not allow getting reports for multiple requests with different ids, so we have to
+    make these requests one by one
      */
 
     //make requests
@@ -134,8 +95,8 @@ class AnalyticsReportServiceImpl @Inject()(
     val requestForUsers: GetReportsRequest = new GetReportsRequest().setReportRequests(java.util.Arrays.asList(newReportRequest.setViewId(VIEW_ID_LOGGED_IN_USER)))
 
     //get results
-    val resultForGuests = analytics.reports().batchGet(requestForGuests).execute()
-    val resultForLoggedInUsers = analytics.reports().batchGet(requestForUsers).execute()
+    val resultForGuests = analyticsTransport.getReports(requestForGuests)
+    val resultForLoggedInUsers = analyticsTransport.getReports(requestForUsers)
 
     //get reports
     val reportForGuests = resultToRows(resultForGuests)
@@ -146,9 +107,19 @@ class AnalyticsReportServiceImpl @Inject()(
 
     //map to Seq[CombinedRow]
     allIds.map { id =>
-      val guestCount =  reportForGuests.find(_.id == id).map(_.count).getOrElse(0)
+      val guestCount = reportForGuests.find(_.id == id).map(_.count).getOrElse(0)
       val userCount = reportForUsers.find(_.id == id).map(_.count).getOrElse(0)
       CombinedRow(id, guestCount, userCount)
+    }
+  }
+
+  def resultToRows(response: GetReportsResponse) = response.getReports.asScala.flatMap { report =>
+    val rows = Option(report.getData.getRows)
+    rows.map(_.asScala).getOrElse(Nil).map { row =>
+      Row(
+        row.getDimensions.get(0).split("/")(2), // get newsId substring from pagePath
+        row.getMetrics.get(0).getValues.get(0).toInt
+      )
     }
   }
 
---
+++ app/services/analytics/AnalyticsTransport.scala
@@ -0,0 +1,51 @@
+package services.analytics
+
+import java.io.File
+import javax.inject.Singleton
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.analyticsreporting.v4.{AnalyticsReporting, AnalyticsReportingScopes}
+import com.google.api.services.analyticsreporting.v4.model._
+import com.google.inject.{ImplementedBy, Inject}
+import play.api.Configuration
+
+@ImplementedBy(classOf[GoogleAnalyticsTransport])
+trait AnalyticsTransport {
+  def getReports(requests: GetReportsRequest): GetReportsResponse
+}
+
+@Singleton
+class GoogleAnalyticsTransport @Inject()(
+  config: Configuration
+) extends AnalyticsTransport {
+
+
+  private val APPLICATION_NAME = "Start"
+  private val JSON_FACTORY = new JacksonFactory()
+
+  private val KEY_FILE_LOCATION = config.getString("start.analytics.key_path")
+    .getOrElse(throw new IllegalStateException("Missing Google Analytics key file path - set start.analytics.key_path"))
+  private val SERVICE_ACCOUNT_EMAIL = config.getString("start.analytics.account.email")
+    .getOrElse(throw new IllegalStateException("Missing Google Analytics service account id - set start.analytics.account.email"))
+
+  private val analytics: AnalyticsReporting = {
+    val httpTransport = GoogleNetHttpTransport.newTrustedTransport()
+    val credential = new GoogleCredential.Builder()
+      .setTransport(httpTransport)
+      .setJsonFactory(JSON_FACTORY)
+      .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
+      .setServiceAccountPrivateKeyFromP12File(new File(KEY_FILE_LOCATION))
+      .setServiceAccountScopes(AnalyticsReportingScopes.all())
+      .build()
+
+    new AnalyticsReporting.Builder(httpTransport, JSON_FACTORY, credential)
+      .setApplicationName(APPLICATION_NAME).build()
+  }
+
+  override def getReports(request: GetReportsRequest) = {
+    analytics.reports().batchGet(request).execute()
+  }
+}
+
