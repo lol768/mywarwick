54425411b7da79a079aeca26a747adee76dbd314
kai@shylock <k.lan@warwick.ac.uk>
2016-09-30 10:36:58 +0100

    NEWSTART-454 removed unnecessary implicit parameters, which caused compilation failure

--- app/services/analytics/NewsAnalyticsService.scala
+++ app/services/analytics/NewsAnalyticsService.scala
@@ -5,14 +5,14 @@ import play.api.mvc.{AnyContent, Request}
 
 @ImplementedBy(classOf[NewsAnalyticsServiceImpl])
 trait NewsAnalyticsService {
-  def getClicks(ids: Seq[String])(implicit request: Request[AnyContent]): Seq[CombinedRow]
+  def getClicks(ids: Seq[String]): Seq[CombinedRow]
 }
 
 class NewsAnalyticsServiceImpl @Inject()(
   analyticsReportService: AnalyticsReportService
 ) extends NewsAnalyticsService {
 
-  override def getClicks(ids: Seq[String])(implicit request: Request[AnyContent]) = {
+  override def getClicks(ids: Seq[String]) = {
     val PAGE_PATH = "pagePath"
     val paths = ids.map(id => controllers.api.routes.ReadNewsController.redirect(id).url)
     val filters = analyticsReportService.idEqualityFilter(paths, PAGE_PATH)
