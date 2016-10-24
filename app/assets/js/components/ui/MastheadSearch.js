ab2c9bd95117c1fd2db8187086462729fac4d5f2
Alec Cursley <a.cursley@warwick.ac.uk>
2016-10-24 12:38:03 +0100

    NEWSTART-429 Use standard ID7 search field on desktop

--- app/assets/js/components/ui/MastheadSearch.js
+++ app/assets/js/components/ui/MastheadSearch.js
@@ -1,76 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Popover from './Popover';
-import SearchField from './SearchField';
-import SearchView from '../views/SearchView';
-import $ from 'jquery';
 
 export default class MastheadSearch extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      query: '',
-      hasFocus: false,
-    };
-
-    this.onChange = this.onChange.bind(this);
-    this.onFocus = this.onFocus.bind(this);
-    this.onSearch = this.onSearch.bind(this);
-    this.onDocumentClick = this.onDocumentClick.bind(this);
-  }
-
-  componentWillUnmount() {
-    $(document).off('click', this.onDocumentClick);
-  }
-
-  onChange(query) {
-    if (query.trim().length === 0) {
-      this.setState({ query });
-    }
-  }
-
-  onSearch(query) {
-    this.setState({ query });
-  }
-
-  onDocumentClick(e) {
-    const thisNode = ReactDOM.findDOMNode(this);
-
-    const parents = $(e.target).parents().get();
-    if (parents.indexOf(thisNode) >= 0) return;
-
-    $(document).off('click', this.onDocumentClick);
-
-    this.setState({ hasFocus: false });
-  }
-
-  onFocus() {
-    this.setState({ hasFocus: true });
-
-    $(document).on('click', this.onDocumentClick);
-  }
-
   render() {
-    const { hasFocus, query } = this.state;
-
     return (
       <div className="id7-search-column">
-        <SearchField
-          onChange={ this.onChange }
-          onFocus={ this.onFocus }
-          onSearch={ this.onSearch }
-          ref="field"
-        />
-        { hasFocus ?
-          <Popover
-            attachTo={this.refs.field.refs.input} top={42} left={20}
-            width={$(this.refs.field.refs.input).outerWidth() - 1} height={500}
-          >
-            <SearchView query={query} showSearchForm={false} />
-          </Popover>
-          : null }
+        <div className="id7-search">
+          <form action="//search.warwick.ac.uk/website" role="search">
+            <div className="form-group">
+              <label className="sr-only" htmlFor="id7-search-box">Search</label>
+              <div className="id7-search-box-container">
+                <input type="search" className="form-control input-lg" id="id7-search-box"
+                  name="q" placeholder="Search Warwick" data-suggest="go"
+                />
+                <i className="fa fa-search fa-2x"></i>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
