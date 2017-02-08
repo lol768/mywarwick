b076854a29fa13885ea1ec8b6d72f055a81a3c94
Nick Howes <n.howes@warwick.ac.uk>
2017-02-08 15:28:39 +0000

    NEWSTART-497 Wrap health checks in try block.
    A few tweaks to allow the test to work.

---
+++ test/services/HealthCheckServiceTest.scala
@@ -0,0 +1,37 @@
+package services
+
+import akka.actor.ActorSystem
+import helpers.WithActorSystem
+import org.mockito.Mockito
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import services.messaging.MessagingService
+import org.mockito.Mockito._
+import org.mockito.Mockito.{atLeast => atLeastTimes}
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Millis, Span}
+
+import scala.concurrent.duration._
+
+/**
+  *
+  */
+class HealthCheckServiceTest extends PlaySpec with MockitoSugar with WithActorSystem with Eventually {
+  override implicit def patienceConfig = PatienceConfig(scaled(Span(500, Millis)), scaled(Span(10, Millis)))
+
+  "HealthCheckService" should {
+    "continue after an exception" in {
+      val messaging = mock[MessagingService]
+      when(messaging.getQueueStatus()).thenReturn(Nil)
+      when(messaging.getOldestUnsentMessageCreatedAt()).thenThrow(new RuntimeException("DB ERROR"))
+      val service = new HealthCheckService(messaging, akka) {
+        override def frequency: FiniteDuration = 1.millis
+      }
+
+      eventually {
+        // if it crashed, it will only ever run 1 time.
+        verify(messaging, atLeastTimes(2)).getQueueStatus()
+      }
+    }
+  }
+}
