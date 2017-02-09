1b2bb5c64e912c87f362b1fca7c43f4377f98c7d
kai@shylock <k.lan@warwick.ac.uk>
2017-02-09 16:47:02 +0000

    NEWSTART-178 allow tile content service to return all preference options

--- app/services/TileContentService.scala
+++ app/services/TileContentService.scala
@@ -8,13 +8,14 @@ import com.fasterxml.jackson.core.JsonProcessingException
 import com.google.common.base.Charsets
 import com.google.common.io.CharStreams
 import com.google.inject.ImplementedBy
-import models.{API, TileInstance}
+import models.{API, Tile, TileInstance}
 import org.apache.http.client.config.RequestConfig
 import org.apache.http.client.methods._
 import org.apache.http.conn.HttpHostConnectException
 import org.apache.http.entity.{ContentType, StringEntity}
 import org.apache.http.impl.client.HttpClientBuilder
 import play.api.libs.json.{JsObject, _}
+import play.api.libs.ws.WSClient
 import system.{Logging, ThreadPools}
 import uk.ac.warwick.sso.client.trusted.{CurrentApplication, TrustedApplicationUtils}
 import warwick.sso.User
@@ -33,12 +34,15 @@ object TileContentService {
 @ImplementedBy(classOf[TileContentServiceImpl])
 trait TileContentService {
 
+  def getTilesOptions(tiles: Seq[Tile]): Future[JsValue]
+
   def getTileContent(user: Option[User], tileInstance: TileInstance): Future[API.Response[JsObject]]
 
 }
 
 class TileContentServiceImpl @Inject()(
-  trustedApp: CurrentApplication
+  trustedApp: CurrentApplication,
+  ws: WSClient
 ) extends TileContentService with Logging {
 
   import TileContentService._
@@ -54,6 +58,25 @@ class TileContentServiceImpl @Inject()(
     .build()
 
   import ThreadPools.tileData
+
+  override def getTilesOptions(tiles: Seq[Tile]): Future[JsValue] = {
+    val allOptions: Seq[Future[(String, JsValue)]] = tiles.map { tile =>
+      tile.fetchUrl match {
+        case Some(url) => {
+          ws.url(s"${url}/preferences.json")
+            .get()
+            .map(res =>
+              res.status match {
+                case 200 => (tile.id, res.json)
+                case _ => (tile.id, Json.obj())
+              }
+            )
+        }
+        case None => Future((tile.id, Json.obj()))
+      }
+    }
+    Future.sequence(allOptions).map(JsObject.apply)
+  }
 
   // TODO cache
   override def getTileContent(user: Option[User], tileInstance: TileInstance): Future[API.Response[JsObject]] =
