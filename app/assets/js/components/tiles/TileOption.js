4b6d12d9f661730181d6c1d7bb0b470dc9644a45
kai@shylock <k.lan@warwick.ac.uk>
2017-02-10 14:36:45 +0000

    NEWSTART-178 tileOption component wip

---
+++ app/assets/js/components/tiles/TileOption.js
@@ -0,0 +1,11 @@
+// import React, { Component } from 'react';
+//
+// export default class TileOption extends Component {
+//
+//   constructor(props) {
+//     super(props);
+//   }
+//
+//
+//
+// }
