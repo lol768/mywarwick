70c4690118f674cd84cce17caeded9b6406f64cf
Nick Howes <n.howes@warwick.ac.uk>
2017-02-24 19:40:08 +0000

    NEWSTART-532 Only setUser when we have a User.

--- app/assets/js/bridge.js
+++ app/assets/js/bridge.js
@@ -23,8 +23,12 @@ export default function init(opts) {
   function doInit(native) {
     const nativeSelectors = [
       createSelector(
-        state => state.user.data,
-        user => native.setUser(user)
+        state => state.user,
+        user => {
+          if (!user.empty) {
+            native.setUser(user.data);
+          }
+        }
       ),
       createSelector(
         state => _.get(state, 'routing.locationBeforeTransitions.pathname', '/'),
