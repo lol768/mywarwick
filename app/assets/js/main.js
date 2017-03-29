eac78822f37568b97949e6a106330c26e09f5f43
Alec Cursley <a.cursley@warwick.ac.uk>
2017-03-29 10:08:41 +0100

    NEWSTART-615

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -199,7 +199,7 @@ function receiveUserInfo(response) {
       if (data.refresh) {
         window.location = user.rewriteRefreshUrl(data.refresh, window.location.href);
       } else {
-        if (!data.user.authenticated) {
+        if (!data.user.authenticated && false) {
           window.location = data.links.permissionDenied;
         } else {
           store.dispatch(user.userReceive(data.user));
