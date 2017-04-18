e60b3bee32561cf945475b93cfe5073f83631b20
kai@shylock <k.lan@warwick.ac.uk>
2017-04-18 16:16:41 +0100

    NEWSTART-651 different messages for different types of information

--- app/assets/js/components/ui/noMoreItem.js
+++ app/assets/js/components/ui/noMoreItem.js
@@ -1,16 +1,26 @@
 import React, { PropTypes } from 'react';
 
-export default class NoMoreItem extends React.Component {
+export default class NoMoreItem extends React.PureComponent {
 
   static propTypes = {
     visible: PropTypes.bool,
+    type: PropTypes.String,
   };
+
+  constructor(props) {
+    super(props);
+    this.phrases = {
+      ActivityView: 'There are no older activities.',
+      NotificationsView: 'There are no older notifications.',
+      NewsView: 'There is no older news.',
+    };
+  }
 
   render() {
     return (
-      <div className="centered">
+      <div className="centered empty-state">
         { this.props.visible ?
-          <span className="label label-info" id="no_more_item">No more item</span> : '' }
+          <p className="lead" id="no_more_item">{ this.phrases[this.props.type] }</p> : '' }
       </div>
     );
   }
