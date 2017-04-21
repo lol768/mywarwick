9d4ff46e65c35e3e79c7408f81296fdf62bbb2c0
Matthew Jones <matthew.jones@warwick.ac.uk>
2017-04-21 14:05:39 +0100

    NEWSTART-603 - Parse stack trace from JS errors

--- app/controllers/api/ErrorsController.scala
+++ app/controllers/api/ErrorsController.scala
@@ -4,8 +4,11 @@ import javax.inject.Singleton
 
 import controllers.BaseController
 import play.api.Logger
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsNumber, JsString, JsValue}
 import play.api.mvc.Action
+
+import scala.util.{Failure, Try}
+import scala.util.matching.Regex
 
 @Singleton
 class ErrorsController extends BaseController {
@@ -14,11 +17,59 @@ class ErrorsController extends BaseController {
 
   def js = Action { implicit request =>
     request.body.asJson.flatMap(_.validate[Seq[Map[String, JsValue]]].asOpt).toSeq.flatten.foreach { error =>
-      val values = error.map { case (k, v) => s"$k=$v" }.mkString(" ")
-
-      logger.error(values)
+      Try(new JsException(error))
+        .recoverWith {
+          case parseException =>
+            logger.error(parseException.getMessage, parseException)
+            Failure(parseException)
+        }
+        .toOption
+        .map { jsException =>
+          logger.error(jsException.getMessage, jsException)
+          jsException
+        }.getOrElse(
+          logger.error(error.map { case (k, v) => s"$k=$v" }.mkString(" "))
+        )
     }
+
     Ok("")
+  }
+
+}
+
+object JsException {
+  val lineRegex: Regex = "^\\s*at\\s(.+)(?:\\.(.+))*\\s\\((.+):(\\d+):\\d+\\)".r
+}
+
+class JsException(errorMap: Map[String, JsValue]) extends RuntimeException {
+
+  private val (message, stack, source, line) = {
+    val seq = Seq("message", "stack", "source", "line").map(k =>
+      errorMap.getOrElse(k, throw new IllegalArgumentException(s"Could not find $k in error map"))
+    )
+    seq match {
+      case Seq(a: JsString, b: JsString, c: JsString, d: JsNumber) =>
+        (a.value, b.value, c.value, d.value.intValue())
+      case _ =>
+        throw new IllegalArgumentException()
+    }
+  }
+
+  private val stacktrace =
+    Seq(new StackTraceElement("", "", source, line)) ++
+      stack.split("\n").drop(1).map {
+        case JsException.lineRegex(className, method, fileName, lineNumber) =>
+          new StackTraceElement(className, Option(method).getOrElse(""), fileName, lineNumber.toInt)
+        case stackLine =>
+          throw new IllegalArgumentException(s"Could not parse line $stackLine")
+      }
+
+  override def getMessage: String = {
+    message
+  }
+
+  override def getStackTrace: Array[StackTraceElement] = {
+    stacktrace.toArray
   }
 
 }
