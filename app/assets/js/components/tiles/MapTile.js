84cd79b2c0c5df30b40aaffcb261c16f467039c0
Alec Cursley <a.cursley@warwick.ac.uk>
2017-05-03 12:22:42 +0100

    NEWSTART-662 Display current location on map tile

--- app/assets/js/components/tiles/MapTile.js
+++ app/assets/js/components/tiles/MapTile.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import TileContent from './TileContent';
+import log from 'loglevel';
 
 export default class MapTile extends TileContent {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasGeolocation: 'geolocation' in navigator,
+      position: null,
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.hasGeolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.setState({
+            position: {
+              longitude: pos.coords.longitude,
+              latitude: pos.coords.latitude,
+            },
+          });
+        },
+        (e) => {
+          log.error('Error returned from geolocation', e);
+
+          this.setState({
+            position: null,
+          })
+        },
+        {
+          enableHighAccuracy: true,
+        });
+    }
+  }
 
   static canZoom() {
     return true;
   }
 
   getLargeBody() {
+    if (this.state.position) {
+      const { latitude, longitude } = this.state.position;
+      const width = 400, height = 300;
+      const image = `/service/map/${latitude.toFixed(5)}/${longitude.toFixed(5)}/${width}/${height}`;
+
+      return <img src={image} className="map-tile-image"/>;
+    }
+
     return null;
   }
 
