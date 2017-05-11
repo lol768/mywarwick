6b354bb4ba833ecdce0f864571e151c1b7f1dcee
Matthew Jones <matthew.jones@warwick.ac.uk>
2017-05-11 09:21:48 +0100

    NEWSTART-674 - Don't show Older if it's the only group

---
+++ test/assets/js/components/views/NotificationsView_test.js
@@ -0,0 +1,62 @@
+import NotificationsView from 'components/views/NotificationsView';
+import * as React from 'react';
+import moment from 'moment';
+import * as enzyme from 'enzyme';
+import { mergeNotifications } from 'state/notifications';
+
+describe('NotificationsView', () => {
+
+  it('only shows Older if it\'s not the only group', () => {
+    const ungroupedProps = {
+      notifications: mergeNotifications({}, [
+          {
+            id: '123',
+            notification: true,
+            provider: 'foo',
+            type: 'foo',
+            title: 'title',
+            text: 'text',
+            date: moment().subtract(1, 'months').toISOString()
+          },
+      ]),
+      dispatch: () => {},
+      notificationsLastRead: {
+        fetched: true,
+        date: moment(),
+      },
+    };
+    const ungrouped = enzyme.render(<NotificationsView.WrappedComponent {...ungroupedProps} />);
+    expect(ungrouped.find('.list-group')).to.have.length(0);
+
+    const groupedProps = {
+      notifications: mergeNotifications({}, [
+        {
+          id: '123',
+          notification: true,
+          provider: 'foo',
+          type: 'foo',
+          title: 'title',
+          text: 'text',
+          date: moment().subtract(1, 'months').toISOString()
+        },
+        {
+          id: '234',
+          notification: true,
+          provider: 'foo',
+          type: 'foo',
+          title: 'title',
+          text: 'text',
+          date: moment().toISOString()
+        }
+      ]),
+      dispatch: () => {},
+      notificationsLastRead: {
+        fetched: true,
+        date: moment(),
+      },
+    };
+    const grouped = enzyme.render(<NotificationsView.WrappedComponent {...groupedProps} />);
+    expect(grouped.find('.list-group')).to.have.length(2);
+  });
+
+});
