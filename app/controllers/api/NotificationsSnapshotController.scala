1be9607675e957dc9e118a484b0df36acd4b8661
Adam Williams <A.Williams.9@warwick.ac.uk>
2017-06-21 11:46:33 +0100

    Fix up CORS

--- app/controllers/api/NotificationsSnapshotController.scala
+++ app/controllers/api/NotificationsSnapshotController.scala
@@ -3,12 +3,10 @@ package controllers.api
 import javax.inject.{Inject, Singleton}
 
 import controllers.BaseController
-import org.joda.time.DateTime
 import play.api.db.{Database, NamedDatabase}
 import play.api.libs.json.Json
 import play.api.mvc.Action
 import services.ActivityService
-import services.dao.ActivityDao
 import uk.ac.warwick.userlookup.UserLookupInterface
 import warwick.sso.Usercode
 
@@ -16,6 +14,12 @@ import warwick.sso.Usercode
 class NotificationsSnapshotController @Inject() (@NamedDatabase("default") db: Database, lookup: UserLookupInterface, activityService: ActivityService) extends BaseController {
 
   val WARWICK_SSO_COOKIE_NAME: String = "WarwickSSO"
+
+  def invalidOrigin(headerOption: Option[String]): Boolean = {
+    headerOption.fold(true)(headerToCheck => {
+      !(headerToCheck.startsWith("https://") && headerToCheck.endsWith(".warwick.ac.uk"))
+    })
+  }
 
   /**
     * We need to be able to support users who don't have a session,
@@ -26,18 +30,23 @@ class NotificationsSnapshotController @Inject() (@NamedDatabase("default") db: D
     val cookie = request.cookies.get(WARWICK_SSO_COOKIE_NAME)
     val optionUser = cookie.map(_.value).map(lookup.getUserByToken)
 
-    if (optionUser.isEmpty) {
-      Forbidden("You must possess a valid WarwickSSO cookie to do this, sorry.")
+    val originHeader = request.headers.get(ORIGIN)
+    if (optionUser.isEmpty || invalidOrigin(originHeader)) {
+      Forbidden("Not permitted.")
     } else {
       val user = optionUser.get
       val userCode = user.getUserId
       db.withConnection { implicit c =>
         Ok(Json.obj(
           "unreads" -> activityService.countUnreadNotificationsForUsercode(Usercode(userCode))
-        ))
+        )).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> originHeader.get)
+          .withHeaders(ACCESS_CONTROL_ALLOW_METHODS -> "GET")
+          .withHeaders(ACCESS_CONTROL_ALLOW_CREDENTIALS -> "true")
+          .withHeaders(VARY -> ORIGIN)
       }
     }
 
   }
+
 }
 
