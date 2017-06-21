02641f028129dcf8860dd89ce4c0a749bb798753
Adam Williams <A.Williams.9@warwick.ac.uk>
2017-06-21 11:06:32 +0100

    Take 1, using DAO at the moment

---
+++ app/controllers/api/NotificationsSnapshotController.scala
@@ -0,0 +1,43 @@
+package controllers.api
+
+import javax.inject.{Inject, Singleton}
+
+import controllers.BaseController
+import org.joda.time.DateTime
+import play.api.db.{Database, NamedDatabase}
+import play.api.libs.json.Json
+import play.api.mvc.Action
+import services.dao.ActivityDao
+import uk.ac.warwick.userlookup.UserLookupInterface
+
+@Singleton
+class NotificationsSnapshotController @Inject() (@NamedDatabase("default") db: Database, lookup: UserLookupInterface, activityDao: ActivityDao) extends BaseController {
+
+  val WARWICK_SSO_COOKIE_NAME: String = "WarwickSSO"
+
+  /**
+    * We need to be able to support users who don't have a session,
+    * so we use the old-style `WarwickSSO` cookie token here. This isn't
+    * ideal, but the data returned is of trivial significance.
+    */
+  def unreads = Action { implicit request =>
+    val cookie = request.cookies.get(WARWICK_SSO_COOKIE_NAME)
+    val optionUser = cookie.map(_.value).map(lookup.getUserByToken)
+
+    if (optionUser.isEmpty) {
+      Forbidden("You must possess a valid WarwickSSO cookie to do this, sorry.")
+    } else {
+      val user = optionUser.get
+      val userCode = user.getUserId
+      db.withConnection { implicit c =>
+        val concreteLastReadDate = activityDao.getLastReadDate(userCode).getOrElse(new DateTime(0))
+
+        Ok(Json.obj(
+          "unreads" -> activityDao.countNotificationsSinceDate(userCode, concreteLastReadDate)
+        ))
+      }
+    }
+
+  }
+}
+
