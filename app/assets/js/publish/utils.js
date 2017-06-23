a3bf54f6a1b6f37f0c131206eed43e32478a4ebd
kai@shylock <k.lan@warwick.ac.uk>
2017-06-23 10:11:21 +0100

    NEWSTART-772 add bluebird for supporting promise

--- app/assets/js/publish/utils.js
+++ app/assets/js/publish/utils.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import 'jquery-form/jquery.form.js';
+import Promise from 'bluebird';
 
 export function promiseSubmit(form, options = {}) {
   return new Promise((resolve, reject) =>
