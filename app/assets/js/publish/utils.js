6546294677236cbe53e7431cf75607f81c10d075
kai@shylock <k.lan@warwick.ac.uk>
2017-06-23 10:23:44 +0100

    NEWSTART-772 don't have to use bluebird, found nick made a handy one I can import

--- app/assets/js/publish/utils.js
+++ app/assets/js/publish/utils.js
@@ -1,6 +1,6 @@
+import '../polyfills-and-errors';
 import $ from 'jquery';
 import 'jquery-form/jquery.form.js';
-import Promise from 'bluebird';
 
 export function promiseSubmit(form, options = {}) {
   return new Promise((resolve, reject) =>
