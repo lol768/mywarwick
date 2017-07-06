61a9505fafcb4795c05d28ad1bf5b21b43d758cf
Adam Williams <A.Williams.9@warwick.ac.uk>
2017-07-06 15:53:33 +0100

    Fix up the evolution for FKCs

--- conf/evolutions/default/63.sql
+++ conf/evolutions/default/63.sql
@@ -1,4 +1,9 @@
 # --- !Ups
+delete from user_tile where tile_id in ('activity', 'news', 'notifications');
+delete from user_tile_layout where tile_id in ('activity', 'news', 'notifications');
+
+delete from tile_group_layout where tile_id in ('activity', 'news', 'notifications');
+delete from tile_group where tile_id in ('activity', 'news', 'notifications');
 delete from tile where id in ('activity', 'news', 'notifications');
 
 # --- !Downs
