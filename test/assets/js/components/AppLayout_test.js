2e1e3e6dc53794bd77b1efaa5c800b2f7cf77aa0
Adam Williams <A.Williams.9@warwick.ac.uk>
2017-07-24 14:05:53 +0100

    Fix test

--- test/assets/js/components/AppLayout_test.js
+++ test/assets/js/components/AppLayout_test.js
@@ -25,7 +25,6 @@ describe('AppLayout', () => {
           title="Activity" icon="tachometer" selectedIcon="tachometer" path="/activity"
           isDisabled={ true }
         />
-        <TabBarItem title="News" icon="newspaper-o" selectedIcon="newspaper-o" path="/news" />
         <TabBarItem title="Search" icon="search" selectedIcon="search" path="/search" />
       </TabBar>
     );
