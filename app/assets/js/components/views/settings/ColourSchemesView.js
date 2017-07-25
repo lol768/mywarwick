ac81a00615705be8e6db1fd349e8de95050a5cfd
kai@shylock <k.lan@warwick.ac.uk>
2017-07-25 13:47:34 +0100

    NEWSTART-917 NEWSTART-915 option view for colour schemes

--- app/assets/js/components/views/settings/ColourSchemesView.js
+++ app/assets/js/components/views/settings/ColourSchemesView.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
 import HideableView from '../HideableView';
+import _ from 'lodash-es';
+import RadioListGroupItem from '../../ui/RadioListGroupItem';
 import { connect } from 'react-redux';
 
-class ColourSchemesView extends HideableView {
+class ColourSchemesView {
   static propTypes = {
     fetching: PropTypes.bool.isRequired,
     failed: PropTypes.bool.isRequired,
@@ -14,7 +16,62 @@ class ColourSchemesView extends HideableView {
       url: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
     }).isRequired,
+    isOnline: PropTypes.bool.isRequired,
   };
+
+  makeRadioItem(choice, groupName) {
+    const chosen = this.props.chosen;
+    return (
+      <RadioListGroupItem
+        key={ `${name}:${choice.id}` }
+        icon={ `cog` }
+        description={ choice.name }
+        onClick={ this.onClick }
+        checked={ choice.id === chosen }
+        name={ groupName }
+        value={ choice.id }
+        disabled={ !this.props.isOnline }
+      />
+    );
+  }
+
+  makeItem(scheme) {
+    return (
+      <div
+        className="list-group-item"
+        role="button"
+        onClick={ this.onClick }
+      >
+        <div className="media">
+
+
+          <div className="media-left">
+            <div className="md-radio">
+              <input
+                type="radio"
+                checked={ scheme.id === this.props.chosen }
+                readOnly
+                disabled={ !this.props.isOnline }
+              />
+              <label />
+            </div>
+          </div>
+
+          <div
+            className={ classNames('media-body', { 'media-body-disabled': !this.props.isOnline })}>
+            { scheme.name }
+          </div>
+          {scheme.name}
+          <div className="media-right">
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  onClick() {
+
+  }
 
   render() {
     return (
@@ -28,23 +85,9 @@ class ColourSchemesView extends HideableView {
         </div>
 
         <div className="list-group">
-          <div
-            className="list-group-item setting-colour-1"
-            role="button"
-            tabIndex={0}
-          >
-            <div className="media">
-              <div className="media-left">
-                <i className="fa fa-fw fa-sun-o" />
-              </div>
-              <div className="media-body">
-                Some media body!
-              </div>
-              <div className="media-right">
-                <i className="fa fa-fw fa-chevron-right" />
-              </div>
-            </div>
-          </div>
+          {_.map(this.props.schemes, (scheme) => {
+            return this.makeItem(scheme);
+          })}
         </div>
       </div>
     );
@@ -58,6 +101,7 @@ function select(state) {
     fetched: state.colourSchemes.fetched,
     chosen: state.colourSchemes.chosen,
     schemes: state.colourSchemes.schemes,
+    isOnline: state.device.isOnline,
   };
 }
 
