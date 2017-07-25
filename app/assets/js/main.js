8560f4dc198b321a0dec5ddf709192f707710e02
kai@shylock <k.lan@warwick.ac.uk>
2017-07-25 10:14:12 +0100

    NEWSTART-917 NEWSTART-915 hook up to persisted

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -26,6 +26,7 @@ import * as user from './state/user';
 import * as news from './state/news';
 import * as newsCategories from './state/news-categories';
 import * as newsOptIn from './state/news-optin';
+import * as colourSchemes from './state/colour-schemes';
 import * as ui from './state/ui';
 import * as device from './state/device';
 import * as analytics from './analytics';
@@ -163,6 +164,9 @@ export function launch(userData) {
 
   persisted('tiles.data', tiles.fetchedTiles);
   persisted('tileContent', tiles.loadedAllTileContent);
+  persisted('tileContent', tiles.loadedAllTileContent);
+  persisted('colourSchemes', colourSchemes.receive);
+
 
   persisted(
     'newsCategories',
@@ -191,6 +195,7 @@ export function launch(userData) {
 
   const loadDataFromServer = _.once(() => {
     store.dispatch(tiles.fetchTiles());
+    store.dispatch(colourSchemes.fetch());
 
     if (hasAuthoritativeAuthenticatedUser(store.getState())) {
       store.dispatch(notifications.fetch());
