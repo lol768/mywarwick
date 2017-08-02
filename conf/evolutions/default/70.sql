eb8872d28fdcf24caabc323c3cedc86203a328aa
Adam Williams <A.Williams.9@warwick.ac.uk>
2017-08-02 16:54:59 +0100

    int -> number(1)

--- conf/evolutions/default/70.sql
+++ conf/evolutions/default/70.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-alter table USER_PREFERENCE add CHOSEN_COLOUR_SCHEME int default 1 not null;
+alter table USER_PREFERENCE add CHOSEN_COLOUR_SCHEME number(1) default 1 not null;
 
 # --- !Downs
 alter table USER_PREFERENCE drop column CHOSEN_COLOUR_SCHEME;
