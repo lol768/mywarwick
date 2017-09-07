77f22174086f0cef724b2080c4e516bed88f4e81
kai@shylock <k.lan@warwick.ac.uk>
2017-09-07 15:50:15 +0100

    NEWSTART-1017 add audience component to the data sent to es

--- app/services/elasticSearch/ActivityESService.scala
+++ app/services/elasticSearch/ActivityESService.scala
@@ -3,13 +3,13 @@ package services.elasticSearch
 import javax.inject.{Inject, Singleton}
 
 import com.google.inject.ImplementedBy
-import models.Activity
+import models.Audience.{DepartmentAudience, ModuleAudience, UsercodeAudience, WebGroupAudience}
+import models.{Activity, Audience}
 import models.publishing.Publisher
 import org.elasticsearch.action.ActionListener
 import org.elasticsearch.action.index.{IndexRequest, IndexResponse}
 import org.joda.time.DateTime
 import services.{AudienceService, PublisherService}
-import warwick.sso.Usercode
 
 @ImplementedBy(classOf[ActivityESServiceImpl])
 trait ActivityESService {
@@ -25,8 +25,12 @@ class ActivityESServiceImpl @Inject()(
 
   private val client = eSClientConfig.newClient
 
-  def indexNameToday(): String = {
-    s"""activity_${DateTime.now().toString("yyyy_mm_dd")}"""
+  def indexNameToday(isNotification: Boolean): String = {
+    val today = DateTime.now().toString("yyyy_0:MM")
+    isNotification match {
+      case true => s"""alert_$today"""
+      case false => s"""activity_$today"""
+    }
   }
 
   override def index(activity: Activity): Unit = {
@@ -38,11 +42,25 @@ class ActivityESServiceImpl @Inject()(
     val url = activity.url.getOrElse("-")
     val replacedBy = activity.replacedBy.getOrElse("-")
     val publishedAt = activity.publishedAt.toDate
-    val shouldNotify = activity.shouldNotify
-    val audiences: Seq[String] = activity.audienceId match {
-      case Some(id: String) => audienceService.resolve(audienceService.getAudience(id)).getOrElse(Seq(Usercode("-"))).map(_.toString())
+
+    val audienceComponents: Seq[AudienceComponent] = activity.audienceId match {
+      case Some(id: String) => audienceService.getAudience(id).components.map {
+        case e: UsercodeAudience => AudienceComponent("usercode")
+        case e: WebGroupAudience => AudienceComponent("webgroup", e.groupName.string)
+        case e: ModuleAudience => AudienceComponent("module", e.moduleCode)
+        case e: DepartmentAudience => AudienceComponent("department", e.deptCode, e.subset.map(_.entryName.toString))
+        case _ => AudienceComponent()
+      }
+      case _ => Seq(AudienceComponent())
+    }
+
+    val usersInAudience: Seq[String] = activity.audienceId match {
+      case Some(id: String) => audienceService.resolve(audienceService.getAudience(id))
+        .getOrElse(Seq("-"))
+        .map(_.toString.replace("Usercode(", "").replace(")", ""))
       case _ => Seq("-")
     }
+
     val publisher: String = activity.publisherId match {
       case Some(id: String) => publisherService.find(id) match {
         case Some(e: Publisher) => e.id
@@ -55,35 +73,53 @@ class ActivityESServiceImpl @Inject()(
     val builder = jsonBuilder().startObject()
     builder
       .field("activity_id", id)
-      .field("provide_id", providerId)
+      .field("provider_id", providerId)
       .field("type", activityType)
       .field("title", title)
       .field("url", url)
+      .field("test", text)
       .field("replaced_by", replacedBy)
       .field("published_at", publishedAt)
-      .field("should_notify", shouldNotify)
       .field("publisher", publisher)
 
-    builder.startArray("audiences")
-    audiences.foreach(audience => {
+    builder.startArray("audience_users")
+    usersInAudience.foreach(builder.value)
+    builder.endArray()
+
+    builder.startArray("audience_components")
+    audienceComponents.foreach(component => {
       builder.startObject()
-      builder.field("id", audience)
+      builder.field("type", component.kind)
+      builder.field("name", component.name)
+      if (component.departmentSubSet != Nil) {
+        builder.startArray("department_subset")
+        component.departmentSubSet.foreach(builder.value)
+        builder.endArray()
+      }
       builder.endObject()
     })
     builder.endArray()
+
+    //    builder.startArray("audience_components")
+    //    audienceComponents.foreach(builder.value)
+    //    builder.endArray()
+
     builder.endObject()
 
-    val indexRequest = new IndexRequest(indexNameToday(), activityType, id).source(builder)
+    val indexRequest = new IndexRequest(indexNameToday(activity.shouldNotify), activityType, id).source(builder)
 
     client.indexAsync(indexRequest, new ActionListener[IndexResponse] {
       override def onFailure(e: Exception) = {
-
+        println("error sending activity to es")
+        println(e)
       }
 
       override def onResponse(response: IndexResponse) = {
-
+        println("response from es")
+        println(response.toString)
       }
     })
   }
-
 }
+
+sealed case class AudienceComponent(kind: String = "-", name: String = "-", departmentSubSet: Seq[String] = Nil)
