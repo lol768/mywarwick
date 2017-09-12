0b174f36595c18c224d55b511e5a5f98e148067b
kai@shylock <k.lan@warwick.ac.uk>
2017-09-12 14:45:04 +0100

    NEWSTART-1017 use None instead of Option.empty

--- app/services/elasticsearch/ActivityESSearchQuery.scala
+++ app/services/elasticsearch/ActivityESSearchQuery.scala
@@ -3,16 +3,16 @@ package services.elasticsearch
 import java.util.Date
 
 case class ActivityESSearchQuery(
-  provider_id: Option[String] = Option.empty,
-  activity_type: Option[String] = Option.empty,
-  title: Option[String] = Option.empty,
-  url: Option[String] = Option.empty,
-  text: Option[String] = Option.empty,
-  replaced_by: Option[String] = Option.empty,
-  publish_at: Option[ActivityESSearchQuery.DateRange] = Option.empty,
-  publisher: Option[String] = Option.empty,
-  audienceComponents: Option[Seq[String]] = Option.empty,
-  resolvedUsers: Option[Seq[String]] = Option.empty
+  provider_id: Option[String] = None,
+  activity_type: Option[String] = None,
+  title: Option[String] = None,
+  url: Option[String] = None,
+  text: Option[String] = None,
+  replaced_by: Option[String] = None,
+  publish_at: Option[ActivityESSearchQuery.DateRange] = None,
+  publisher: Option[String] = None,
+  audienceComponents: Option[Seq[String]] = None,
+  resolvedUsers: Option[Seq[String]] = None
 )
 
 object ActivityESSearchQuery {
