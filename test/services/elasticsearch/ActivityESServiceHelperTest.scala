5059cc2cbd0e68dfe2255665caafc448b48f8325
kai@shylock <k.lan@warwick.ac.uk>
2017-09-12 14:46:24 +0100

    NEWSTART-1017 fix broken test

--- test/services/elasticsearch/ActivityESServiceHelperTest.scala
+++ test/services/elasticsearch/ActivityESServiceHelperTest.scala
@@ -1,5 +1,7 @@
 package services.elasticsearch
 
+import java.util.Date
+
 import helpers.BaseSpec
 import org.joda.time.DateTime
 import org.scalatest.mockito.MockitoSugar
@@ -31,6 +33,31 @@ class ActivityESServiceHelperTest extends BaseSpec with MockitoSugar {
       ActivityESServiceGetHelper.indexNameForAllTime(false) must be ("activity*")
 
     }
+  }
+
+  "ActivityESServiceHelper" should {
+
+    "make elasticsearch document builder from activity document correctly" in new Scope {
+
+      val activityDoc = ActivityDocument(
+        "test1",
+        "test2",
+        "test3",
+        "test4",
+        "test5",
+        "test6",
+        new Date(0),
+        "test1",
+        Seq("component1", "component2"),
+        Seq("user1", "user2")
+      )
+
+      val result = ActivityESServiceHelper.elasticSearchContentBuilderFromActivityDocument(activityDoc)
+
+      result.string() must be("""[{"provider_id":"test1","activity_type":"test2","title":"test3","url":"test4","text":"test5","replaced_by":"test6","published_at":"1970-01-01T00:00:00.000Z","publisher":"test1","resolved_users":["user1","user2"],"audience_components":["component1","component2"]}]""")
+
+    }
+
   }
 
 }
