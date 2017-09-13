b6b8fabac0cebd69c2588206dce1739f91505bf9
Matthew Jones <matthew.jones@warwick.ac.uk>
2017-09-13 11:51:11 +0100

    NEWSTART-1051 - Native app install banners

--- app/controllers/AppManifestController.scala
+++ app/controllers/AppManifestController.scala
@@ -28,17 +28,24 @@ class AppManifestController @Inject()(
         "theme_color" -> "#8C6E96",
         "icons" -> Json.arr(
           Json.obj(
-            "src" -> "/assets/lib/id7/images/android-chrome-144x144.png",
+            "src" -> "/assets/images/icon-144.png",
             "sizes" -> "144x144",
             "type" -> "image/png"
           ),
           Json.obj(
-            "src" -> "/assets/lib/id7/images/android-chrome-192x192.png",
+            "src" -> "/assets/images/icon-192.png",
             "sizes" -> "192x192",
             "type" -> "image/png"
           )
         ),
-        "gcm_sender_id" -> senderId
+        "gcm_sender_id" -> senderId,
+        "prefer_related_applications" -> true,
+        "related_applications" -> Json.arr(
+          Json.obj(
+            "platform" -> "play",
+            "id" -> "uk.ac.warwick.my.app"
+          )
+        )
       ))
     }
   }
