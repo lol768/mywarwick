d1fbfd89ec89fd17d448c5b647402cb7947a8425
kai@shylock <k.lan@warwick.ac.uk>
2017-09-14 11:37:56 +0100

    NEWSTART-1017 default response handler

--- app/services/elasticsearch/ElasticSearchAdminService.scala
+++ app/services/elasticsearch/ElasticSearchAdminService.scala
@@ -11,6 +11,8 @@ import play.api.libs.json.{JsValue, Json}
 import warwick.core.Logging
 import org.apache.http.entity.ContentType
 
+import scala.concurrent.{Future, Promise}
+
 @ImplementedBy(classOf[ElasticSearchAdminServiceImpl])
 trait ElasticSearchAdminService {
 
@@ -19,13 +21,13 @@ trait ElasticSearchAdminService {
   val templateRootPath = "_template"
 
   // template
-  def putTemplate(template: JsValue, name: String, lowLevelClient: RestClient = lowLevelClient)
+  def putTemplate(template: JsValue, name: String, lowLevelClient: RestClient = lowLevelClient): Future[Response]
 
-  def deleteTemplate(name: String, lowLevelClient: RestClient = lowLevelClient)
+  def deleteTemplate(name: String, lowLevelClient: RestClient = lowLevelClient): Future[Response]
 
-  def getTemplate(name: String, lowLevelClient: RestClient = lowLevelClient)
+  def getTemplate(name: String, lowLevelClient: RestClient = lowLevelClient): Future[Response]
 
-  def hasTemplate(name: String, lowLevelClient: RestClient = lowLevelClient)
+  def hasTemplate(name: String, lowLevelClient: RestClient = lowLevelClient): Future[Response]
 
   // remove index
 
@@ -44,28 +46,52 @@ class ElasticSearchAdminServiceImpl @Inject()(
 
   def httpEntityFromJsValue(json: JsValue) = new NStringEntity(Json.stringify(json), ContentType.APPLICATION_JSON)
 
-  val responseListener = new ResponseListener() {
-    def onFailure(exception: Exception): Unit = {
-
-    }
-
-    override def onSuccess(response: Response) = {
-
+  def defaultResponseHandler(res: Option[Any], responsePromise: Promise[Response]): Unit = {
+    res match {
+      case Some(e: Exception) =>
+        responsePromise.failure(e)
+        logger.error("Exception thrown after sending request to elasticsearch", e)
+      case Some(r: Response) =>
+        responsePromise.success(r)
+        logger.debug(s"Response received from elasticsearch: ${r.toString}")
     }
 
   }
 
-  override def putTemplate(template: JsValue, name: String, lowLevelClient: RestClient): Unit = {
+  def responseListener(
+    responsePromise: Promise[Response],
+    callbackHandler: (Option[Any], Promise[Response]) => Unit = defaultResponseHandler
+  ): ResponseListener = {
+    new ResponseListener() {
+      def onFailure(exception: Exception): Unit = {
+        callbackHandler(Some(exception), responsePromise)
+      }
+
+      override def onSuccess(response: Response) = {
+        callbackHandler(Some(response), responsePromise)
+      }
+    }
+  }
+
+  override def putTemplate(
+    template: JsValue,
+    name: String,
+    lowLevelClient: RestClient
+  ): Future[Response] = {
+    val responsePromise = Promise[Response]
     lowLevelClient.performRequestAsync(
       "PUT",
       s"$templateRootPath/$name",
       emptyParam,
       httpEntityFromJsValue(template),
-      responseListener
+      responseListener(responsePromise)
     )
+    responsePromise.future
   }
 
-  override def deleteTemplate(name: String, lowLevelClient: RestClient): Unit = ???
+  override def deleteTemplate(name: String, lowLevelClient: RestClient): Unit = {
+
+  }
 
   override def getTemplate(name: String, lowLevelClient: RestClient): Unit = ???
 
