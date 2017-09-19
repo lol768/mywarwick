71879490e03aa5003a073c8632689c2a499f74d4
kai@shylock <k.lan@warwick.ac.uk>
2017-09-19 16:47:47 +0100

    NEWSTART-1017 parse csv and init the es client

--- app/services/elasticsearch/ESClient.scala
+++ app/services/elasticsearch/ESClient.scala
@@ -25,20 +25,26 @@ class ESClientConfigImpl @Inject()(
   val lowLevel: RestClientBuilder = RestClient.builder(allHttpHosts.toArray: _*).setMaxRetryTimeoutMillis(60000)
   val highLevel: RestHighLevelClient = new RestHighLevelClient(lowLevel)
 
-  override def nodes: Seq[ESNode] = config
-    .getConfigSeq("es.nodes")
-    .getOrElse(throw new IllegalStateException("ElasticSearch nodes not configured - check es.nodes"))
-    .map(e => {
-      ESNode(
-        e.getString("host") getOrElse (throw new IllegalStateException("ElasticSearch host is missing - check es.nodes")),
-        e.getInt("port").getOrElse(throw new IllegalStateException("ElasticSearch port number is missing - check es.nodes")),
-        e.getString("protocol").getOrElse(throw new IllegalStateException("ElasticSearch protocol is missing - check es.nodes"))
-      )
-    })
+  override def nodes: Seq[ESNode] = {
+    config
+      .getString("es.nodes").map(ESNode.fromConfigs)
+      .getOrElse(throw new IllegalStateException("ElasticSearch nodes not configured - check es.nodes"))
+  }
 
   override def highLevelClient: RestHighLevelClient = highLevel
 
   override def lowLevelClient: RestClient = highLevel.getLowLevelClient
 }
 
-case class ESNode(node: String, port: Int, protocol: String)
+case class ESNode(node: String, port: Int, protocol: String = "http")
+
+object ESNode {
+  def fromSingleConfigString(config: String): ESNode = {
+    val configArr = config.split(":")
+    ESNode(configArr(0).trim, configArr(1).trim.toInt)
+  }
+
+  def fromConfigs(configs: String): Seq[ESNode] = {
+    configs.split(",").toSeq.map(fromSingleConfigString)
+  }
+}
