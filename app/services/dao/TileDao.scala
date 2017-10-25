6891d541f114a9dffd5baaa69831cb950586542e
Kai Lan <k.lan@warwick.ac.uk>
2017-10-25 12:48:07 +0000

    NEWSTART-1081 add the new column to select

--- app/services/dao/TileDao.scala
+++ app/services/dao/TileDao.scala
@@ -50,7 +50,7 @@ class TileDaoImpl @Inject()() extends TileDao {
 
     val userTiles = SQL(
       s"""
-         |SELECT ID, TILE_TYPE, TITLE, ICON, COLOUR, FETCH_URL, REMOVED, PREFERENCES
+         |SELECT ID, TILE_TYPE, TITLE, ICON, COLOUR, TIMEOUT, FETCH_URL, REMOVED, PREFERENCES
          |FROM USER_TILE JOIN TILE ON ID = TILE_ID
          |WHERE USERCODE = {usercode} $idRestriction
       """.stripMargin)
@@ -67,7 +67,7 @@ class TileDaoImpl @Inject()() extends TileDao {
     val idRestriction = if (ids.isEmpty) "" else "AND ID IN ({ids})"
     SQL(
       s"""
-         |SELECT ID, TILE_TYPE, TITLE, ICON, COLOUR, FETCH_URL, 0 AS REMOVED, NULL AS PREFERENCES
+         |SELECT ID, TILE_TYPE, TITLE, ICON, COLOUR, TIMEOUT, FETCH_URL, 0 AS REMOVED, NULL AS PREFERENCES
          |FROM TILE
          |WHERE EXISTS (SELECT * FROM TILE_GROUP WHERE TILE_ID = ID and "GROUP" in ({groups})) $idRestriction
     """.stripMargin).on(
