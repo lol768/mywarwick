29558fc8ed07919d92d1b27aef580dbacbd946ae
Kai Lan <k.lan@warwick.ac.uk>
2017-11-06 14:19:58 +0000

    NEWSTART-1053 helper method for determining iphonex

--- app/assets/js/helpers.js
+++ app/assets/js/helpers.js
@@ -9,3 +9,9 @@
 export function pluralise(unit, count, plural) {
   return count === 1 ? unit : (plural || `${unit}s`);
 }
+
+export function isiPhoneX() {
+  let height = window.screen.height;
+  let width = window.screen.width;
+  return (height === 812 && width === 375)
+}
