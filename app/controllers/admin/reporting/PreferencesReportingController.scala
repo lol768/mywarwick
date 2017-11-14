430463ae385fdf73b89d2da09c57bacdadd4e8fe
Kai Lan <k.lan@warwick.ac.uk>
2017-11-14 14:18:10 +0000

    NEWSTART-1220 basic shape of the preferences reporting controller

---
+++ app/controllers/admin/reporting/PreferencesReportingController.scala
@@ -0,0 +1,25 @@
+package controllers.admin.reporting
+
+import javax.inject.{Inject, Singleton}
+
+import controllers.BaseController
+import play.api.i18n.{I18nSupport, MessagesApi}
+import services.SecurityService
+import services.reporting.PreferencesReportingService
+import system.Roles
+
+@Singleton
+class PreferencesReportingController @Inject()(
+  preferencesReportingService: PreferencesReportingService,
+  securityService: SecurityService,
+  val messagesApi: MessagesApi
+) extends BaseController with I18nSupport {
+
+  import Roles._
+  import securityService._
+
+  def index = RequiredActualUserRoleAction(Sysadmin) { implicit request =>
+    play.api.mvc.Results.Ok(views.html.admin.reporting.preferences.index("test"))
+  }
+
+}
