4b79e45e0639a3d5bd5f7bddd53dd649788993fe
Kai Lan <k.lan@warwick.ac.uk>
2017-11-14 16:46:15 +0000

    NEWSTART-1220 add mutes counts by provider

--- app/services/dao/ActivityMuteDao.scala
+++ app/services/dao/ActivityMuteDao.scala
@@ -30,6 +30,11 @@ trait ActivityMuteDao {
   def mutesForProvider(provider: ActivityProvider)(implicit c: Connection): Seq[ActivityMute]
 
   def mutesForProviders(providers: Seq[ActivityProvider])(implicit c: Connection): Map[ActivityProvider, Seq[ActivityMute]]
+
+  def mutesCountForProvider(provider: ActivityProvider)(implicit c: Connection): Int
+
+  def mutesCountForProviders(providers: Seq[ActivityProvider])(implicit c: Connection): Int
+
 }
 
 class ActivityMuteDaoImpl extends ActivityMuteDao {
@@ -150,6 +155,16 @@ class ActivityMuteDaoImpl extends ActivityMuteDao {
   }
 
   override def mutesForProviders(providers: Seq[ActivityProvider])(implicit c: Connection) = {
-    val p = providers.flatMap { provider => Map(provider -> this.mutesForProvider(provider)) }.toMap
+    providers.flatMap(provider => Map(provider -> this.mutesForProvider(provider))).toMap
+  }
+
+  override def mutesCountForProvider(provider: ActivityProvider)(implicit c: Connection) = {
+    SQL(s"select count(*) from ACTIVITY_MUTE where PROVIDER_ID = {PROVIDER_ID}")
+      .on("PROVIDER_ID" -> provider.id)
+      .as(scalar[Int].single)
+  }
+
+  override def mutesCountForProviders(providers: Seq[ActivityProvider])(implicit c: Connection) = {
+    providers.map(provider => this.mutesCountForProvider(provider)).sum
   }
 }
