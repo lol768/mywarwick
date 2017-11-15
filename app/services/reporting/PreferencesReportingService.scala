e577651f2e5053faebc4c5bd848244b1ab6f0671
Kai Lan <k.lan@warwick.ac.uk>
2017-11-15 10:49:56 +0000

    NEWSTART-1220 preferences reporting service WIP

--- app/services/reporting/PreferencesReportingService.scala
+++ app/services/reporting/PreferencesReportingService.scala
@@ -3,8 +3,9 @@ package services.reporting
 import javax.inject.{Inject, Singleton}
 
 import com.google.inject.ImplementedBy
-import models.{ActivityProvider, ActivityType}
-import org.joda.time.DateTime
+import models.{ActivityMute, ActivityProvider, ActivityType}
+import org.joda.time.Interval
+import play.api.db.{Database, NamedDatabase}
 import services.dao.{ActivityMuteDao, UserPreferencesDao}
 import warwick.sso.Usercode
 
@@ -15,18 +16,18 @@ trait PreferencesReportingService {
 
   def userWantsSMS(usercode: Usercode): Boolean
 
-  def usersWhoMutedProvider(
-    providerId: ActivityProvider,
-    activityType: Option[ActivityType],
-    createdAt: Option[DateTime],
-    expiresAt: Option[DateTime]
-  ): Option[Seq[Usercode]]
+  def getMutesByProviders(providers: Seq[ActivityProvider]): Map[ActivityProvider, Seq[ActivityMute]]
+
+  def getMutesCountByProviders(providers: Seq[ActivityProvider]): Int
+
+  def getAllMutesGroupedByProviders(): Map[ActivityProvider, Seq[ActivityMute]]
 
 }
 
 
 @Singleton
 class PreferencesReportingServiceImpl @Inject()(
+  @NamedDatabase("default") db: Database,
   userPreferencesDao: UserPreferencesDao,
   activityMuteDao: ActivityMuteDao
 ) extends PreferencesReportingService {
@@ -34,5 +35,15 @@ class PreferencesReportingServiceImpl @Inject()(
 
   override def userWantsSMS(usercode: Usercode) = ???
 
-  override def usersWhoMutedProvider(providerId: ActivityProvider, activityType: Option[ActivityType], createdAt: Option[DateTime], expiresAt: Option[DateTime]) = ???
+  override def getMutesByProviders(providers: Seq[ActivityProvider]): Map[ActivityProvider, Seq[ActivityMute]] = {
+    db.withConnection(implicit c => activityMuteDao.mutesForProviders(providers))
+  }
+
+  override def getMutesCountByProviders(providers: Seq[ActivityProvider]) = {
+    db.withConnection(implicit c => activityMuteDao.mutesCountForProviders(providers))
+  }
+
+  override def getAllMutesGroupedByProviders() = {
+    db.withConnection(implicit c => activityMuteDao.allMutesGroupedByProviders())
+  }
 }
