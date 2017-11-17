df8d10487c4608b2dc74fc7ab5186881aa6e786e
Kai Lan <k.lan@warwick.ac.uk>
2017-11-17 16:50:52 +0000

    NEWSTART-1228 handle form submission in controller

--- app/controllers/admin/reporting/ActivityReportingController.scala
+++ app/controllers/admin/reporting/ActivityReportingController.scala
@@ -4,10 +4,14 @@ import javax.inject.{Inject, Singleton}
 
 import controllers.BaseController
 import org.joda.time.{DateTime, Interval}
+import play.api.data.Form
+import play.api.data.Forms.{jodaDate, mapping}
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{AnyContent, Result}
 import services.SecurityService
 import services.reporting.ActivityReportingService
 import system.Roles
+import warwick.sso.AuthenticatedRequest
 
 import scala.collection.immutable.ListMap
 import scala.concurrent.Future
@@ -22,13 +26,37 @@ class ActivityReportingController @Inject()(
   import Roles._
   import securityService._
 
-  // default to report for the past 14 days
-  val defaultReportInterval = new Interval(DateTime.now().minusDays(14), DateTime.now)
+  object FormField {
+    val from = "fromDate"
+    val to = "toDate"
+  }
 
-  def index = RequiredActualUserRoleAction(Sysadmin).async { implicit request =>
+  val formData = Form(
+    mapping(
+      "fromDate" -> jodaDate("yyyy-MM-dd'T'HH:mm:ss"),
+      "toDate" -> jodaDate("yyyy-MM-dd'T'HH:mm:ss")
+    )(ActivityReportFormData.apply)(ActivityReportFormData.unapply) verifying(
+      """"From" date must be before "To" date""",
+      fields => fields match {
+        case data => ActivityReportFormData.validate(data.fromDate, data.toDate).isDefined
+      })
+  )
+
+  def renderResultWithInterval(
+    interval: Interval,
+    formError: Form[ActivityReportFormData] = null
+  )(implicit request: AuthenticatedRequest[AnyContent]): Future[Result] = {
     import system.ThreadPools.elastic
+
+    if (formError != null) {
+      return Future(Ok(views.html.admin.reporting.activity.index(
+        null,
+        formError
+      )))
+    }
+
     for {
-      allAlertsByProviders <- Future.sequence(activityReportingService.allAlertsByProviders(defaultReportInterval).map {
+      allAlertsByProviders <- Future.sequence(activityReportingService.allAlertsByProviders(interval).map {
         case (provider, futureDocs) =>
           for {
             docs <- futureDocs
@@ -37,7 +65,52 @@ class ActivityReportingController @Inject()(
         case (provider, _) => provider.displayName.getOrElse(provider.id)
       })
     } yield {
-      Ok(views.html.admin.reporting.activity.index(ListMap(allAlertsByProviders: _*)))
+      Ok(views.html.admin.reporting.activity.index(
+        ListMap(allAlertsByProviders: _*),
+        formData.fill(ActivityReportFormData(interval.getStart, interval.getEnd))
+      ))
     }
+  }
+
+
+  def index = RequiredActualUserRoleAction(Sysadmin).async { implicit request =>
+    val defaultFormData = ActivityReportFormData.withDefaultTimeDate()
+    //    formData.fill(defaultFormData)
+    renderResultWithInterval(new Interval(defaultFormData.fromDate, defaultFormData.toDate))
+  }
+
+  def formSubmit = RequiredActualUserRoleAction(Sysadmin).async { implicit request =>
+
+    formData.bindFromRequest.fold(
+      formError => {
+        renderResultWithInterval(ActivityReportFormData.makeDefaultTimeDate(), formError)
+      },
+      data => {
+        renderResultWithInterval(new Interval(data.fromDate, data.toDate))
+      }
+    )
+  }
+}
+
+
+case class ActivityReportFormData(fromDate: DateTime, toDate: DateTime)
+
+object ActivityReportFormData {
+  def validate(fromDate: DateTime, toDate: DateTime) = {
+    toDate.isAfter(fromDate) match {
+      case true => Some(ActivityReportFormData(fromDate, toDate))
+      case _ => None
+    }
+  }
+
+  def withDefaultTimeDate() = {
+    val default = makeDefaultTimeDate()
+    ActivityReportFormData(default.getStart, default.getEnd)
+  }
+
+  def makeDefaultTimeDate() = {
+    val defaultTo = DateTime.now()
+    val defaultFrom = defaultTo.minusDays(14)
+    new Interval(defaultFrom, defaultTo)
   }
 }
