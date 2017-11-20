2654d8736af363218a1c84002f61387d4c6c069d
Kai Lan <k.lan@warwick.ac.uk>
2017-11-20 17:11:56 +0000

    NEWSTART-1233 more readable test

--- test/services/dao/ActivityDaoTest.scala
+++ test/services/dao/ActivityDaoTest.scala
@@ -297,8 +297,8 @@ class ActivityDaoTest extends BaseSpec with OneStartAppPerSuite {
     "api" in transaction { implicit c =>
       val activityId = activityDao.save(Fixtures.activitySave.activityFromApi.copy(publishedAt = Some(DateTime.now.minusHours(1))), audienceId, AudienceSize.Public, Nil)
       val saved = activityDao.getActivityById(activityId)
-      saved.isDefined mustBe true
-      saved.orNull.api mustBe true
+      saved must not be empty
+      saved.get.api mustBe true
     }
   }
 }
