410a940eafd062ee0dd20c9a0b2987ff895224ce
Nick Howes <n.howes@warwick.ac.uk>
2017-11-22 14:37:10 +0000

    NEWSTART-1244 Restore JSON bodyparser following ssoclient upgrade.

--- app/controllers/api/IncomingActivitiesController.scala
+++ app/controllers/api/IncomingActivitiesController.scala
@@ -5,12 +5,12 @@ import javax.inject.Singleton
 import com.google.inject.Inject
 import controllers.BaseController
 import models.Audience.UsercodesAudience
-import models.{Audience, _}
 import models.publishing.Ability.CreateAPINotifications
-import play.api.i18n.{I18nSupport, MessagesApi}
+import models.{Audience, _}
+import play.api.i18n.I18nSupport
 import play.api.libs.json._
-import play.api.mvc.{AnyContent, Result}
-import services.ActivityError.{InvalidJSON, InvalidProviderId}
+import play.api.mvc.Result
+import services.ActivityError.InvalidProviderId
 import services._
 import warwick.sso.{AuthenticatedRequest, GroupName, User, Usercode}
 
@@ -24,53 +24,49 @@ class IncomingActivitiesController @Inject()(
 
   import securityService._
 
-  def postActivity(providerId: String) = APIAction { implicit request =>
+  def postActivity(providerId: String) = APIAction(parse.json) { implicit request =>
     postItem(providerId, shouldNotify = false)
   }
 
-  def postNotification(providerId: String) = APIAction { implicit request =>
+  def postNotification(providerId: String) = APIAction(parse.json) { implicit request =>
     postItem(providerId, shouldNotify = true)
   }
 
-  def postItem(providerId: String, shouldNotify: Boolean)(implicit request: AuthenticatedRequest[AnyContent]): Result =
-    request.body.asJson.map { json =>
-      publisherService.getParentPublisherId(providerId) match {
-        case Some(publisherId) =>
-          request.context.user.map { user =>
-            if (publisherService.getRoleForUser(publisherId, user.usercode).can(CreateAPINotifications)) {
-              json.validate[IncomingActivityData].map { data =>
-                val activity = ActivitySave.fromApi(user.usercode, publisherId, providerId, shouldNotify, data)
+  def postItem(providerId: String, shouldNotify: Boolean)(implicit request: AuthenticatedRequest[JsValue]): Result =
+    publisherService.getParentPublisherId(providerId) match {
+      case Some(publisherId) =>
+        request.context.user.map { user =>
+          if (publisherService.getRoleForUser(publisherId, user.usercode).can(CreateAPINotifications)) {
+            request.body.validate[IncomingActivityData].map { data =>
+              val activity = ActivitySave.fromApi(user.usercode, publisherId, providerId, shouldNotify, data)
 
-                val usercodes = data.recipients.users.getOrElse(Seq.empty).map(Usercode) match {
-                  case usercodes: Seq[Usercode] if usercodes.nonEmpty => Seq(UsercodesAudience(usercodes.toSet))
-                  case Nil => Seq.empty[Audience.Component]
-                }
-                val webgroups = data.recipients.groups.getOrElse(Seq.empty).map(GroupName).map(Audience.WebGroupAudience)
+              val usercodes = data.recipients.users.getOrElse(Seq.empty).map(Usercode) match {
+                case usercodes: Seq[Usercode] if usercodes.nonEmpty => Seq(UsercodesAudience(usercodes.toSet))
+                case Nil => Seq.empty[Audience.Component]
+              }
+              val webgroups = data.recipients.groups.getOrElse(Seq.empty).map(GroupName).map(Audience.WebGroupAudience)
 
-                val audience = Audience(usercodes ++ webgroups)
+              val audience = Audience(usercodes ++ webgroups)
 
-                val publisher = publisherService.find(publisherId).get
-                lazy val recipients = audienceService.resolve(audience).toOption.map(_.size).getOrElse(0)
-                publisher.maxRecipients match {
-                  case Some(max) if shouldNotify && recipients > max =>
-                    BadRequest(Json.toJson(API.Failure[JsObject]("bad_request", Seq(API.Error("too-many-recipients", s"You can only send to $max recipients at a time")))))
-                  case _ =>
-                    activityService.save(activity, audience).fold(badRequest, id => {
-                      auditLog('CreateActivity, 'id -> id, 'provider -> activity.providerId)
-                      created(id)
-                    })
-                }
+              val publisher = publisherService.find(publisherId).get
+              lazy val recipients = audienceService.resolve(audience).toOption.map(_.size).getOrElse(0)
+              publisher.maxRecipients match {
+                case Some(max) if shouldNotify && recipients > max =>
+                  BadRequest(Json.toJson(API.Failure[JsObject]("bad_request", Seq(API.Error("too-many-recipients", s"You can only send to $max recipients at a time")))))
+                case _ =>
+                  activityService.save(activity, audience).fold(badRequest, id => {
+                    auditLog('CreateActivity, 'id -> id, 'provider -> activity.providerId)
+                    created(id)
+                  })
+              }
 
-              }.recoverTotal(validationError)
-            } else {
-              forbidden(providerId, user)
-            }
-          }.get // APIAction calls this only if request.context.user is defined
+            }.recoverTotal(validationError)
+          } else {
+            forbidden(providerId, user)
+          }
+        }.get // APIAction calls this only if request.context.user is defined
 
-        case None => badRequest(Seq(InvalidProviderId(providerId)))
-      }
-    }.getOrElse {
-      badRequest(Seq(InvalidJSON))
+      case None => badRequest(Seq(InvalidProviderId(providerId)))
     }
 
   private def badRequest(errors: Seq[ActivityError]): Result =
