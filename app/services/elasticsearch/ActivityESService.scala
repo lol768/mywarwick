c3979463602f7e0a0630a0346df4cd7dc9b41336
Nick Howes <n.howes@warwick.ac.uk>
2017-11-27 16:07:57 +0000

    Remove unused ES search code.
    Don't want anything to just start using the current search functionality as-is, because pagination needs to be added before it can be useful for returning large numbers of results; you would never actually return all the results in one go, you'd return pages of them.

--- app/services/elasticsearch/ActivityESService.scala
+++ app/services/elasticsearch/ActivityESService.scala
@@ -4,7 +4,7 @@ import javax.inject.{Inject, Singleton}
 
 import com.google.inject.ImplementedBy
 import models.Activity
-import org.elasticsearch.action.ActionListener
+import org.elasticsearch.action.{ActionListener, DocWriteRequest}
 import org.elasticsearch.action.bulk.{BulkRequest, BulkRequestBuilder, BulkResponse}
 import org.elasticsearch.action.index.{IndexRequest, IndexResponse}
 import org.elasticsearch.action.search.{SearchRequest, SearchResponse}
@@ -26,12 +26,7 @@ trait ActivityESService {
 
   def index(requests: Seq[IndexActivityRequest]): Future[Unit]
 
-  def deleteDocumentByActivityId(activityId: String, isNotification: Boolean = true)
-
-  def search(activityESSearchQuery: ActivityESSearchQuery): Future[Seq[ActivityDocument]]
-
   def count(activityESSearchQuery: ActivityESSearchQuery): Future[Int]
-
 }
 
 case class IndexActivityRequest(activity: Activity, resolvedUsers: Option[Seq[Usercode]] = None)
@@ -50,11 +45,10 @@ class ActivityESServiceImpl @Inject()(
   private val client: RestHighLevelClient = eSClientConfig.highLevelClient
   private val lowLevelClient: RestClient = eSClientConfig.lowLevelClient
 
-  override def index(req: IndexActivityRequest) = index(Seq(req))
+  override def index(req: IndexActivityRequest): Future[Unit] = index(Seq(req))
 
   def index(reqs: Seq[IndexActivityRequest]): Future[Unit] = {
-    val bulkRequest = new BulkRequest()
-    reqs.foreach { req =>
+    val writeReqs = reqs.map { req =>
       val activity = req.activity
       val resolvedUsers = req.resolvedUsers
       val activityDocument = ActivityDocument.fromActivityModel(
@@ -69,41 +63,12 @@ class ActivityESServiceImpl @Inject()(
 
       val indexName = helper.indexNameForActivity(activity)
 
-      val indexRequest = helper.makeIndexRequest(indexName, helper.documentType, activity.id, docBuilder)
-
-      bulkRequest.add(indexRequest)
+      helper.makeIndexRequest(indexName, helper.documentType, activity.id, docBuilder)
     }
-
+    val bulkRequest = new BulkRequest().add(writeReqs : _*)
     val listener = new FutureActionListener[BulkResponse]
     client.bulkAsync(bulkRequest, listener)
     listener.future.map(_ => {})
-  }
-
-  //TODO implement me https://www.elastic.co/guide/en/elasticsearch/client/java-rest/master/java-rest-high-document-delete.html
-  override def deleteDocumentByActivityId(activityId: String, isNotification: Boolean): Unit = ???
-
-  // TODO we are not using the search function at all at the moent, but if one day we do, this should be reimplemented with search_after to get over the 10k limit
-  // https://www.elastic.co/guide/en/elasticsearch/reference/current/search-request-search-after.html
-  override def search(input: ActivityESSearchQuery): Future[Seq[ActivityDocument]] = {
-    val helper = ActivityESServiceSearchHelper
-
-    val searchRequest = new SearchRequest(ActivityESServiceSearchHelper.indexNameForActivitySearchQuery(input))
-    val boolQueryBuilder: BoolQueryBuilder = helper.makeBoolQueryBuilder(input)
-    val searchSourceBuilder: SearchSourceBuilder = helper.makeSearchSourceBuilder(boolQueryBuilder)
-
-    searchRequest.types(ActivityESServiceSearchHelper.documentType)
-    searchRequest.source(searchSourceBuilder)
-
-    val listener = new FutureActionListener[SearchResponse]
-    client.searchAsync(searchRequest, listener)
-
-    listener.future
-      .map(ActivityDocument.fromESSearchResponse)
-      .recover {
-        case exception =>
-          logger.error("Exceptions thrown after sending a elasticsearch SearchRequest", exception)
-          Seq()
-      }
   }
 
   override def count(input: ActivityESSearchQuery): Future[Int] = {
--- app/services/reporting/ActivityReportingService.scala
+++ app/services/reporting/ActivityReportingService.scala
@@ -8,27 +8,24 @@ import org.joda.time.Interval
 import play.api.db.{Database, NamedDatabase}
 import services.ProviderRender
 import services.dao.PublisherDao
-import services.elasticsearch.{ActivityDocument, ActivityESSearchQuery, ActivityESService}
+import services.elasticsearch.{ActivityESSearchQuery, ActivityESService}
+
 import scala.concurrent.Future
 
 @ImplementedBy(classOf[ActivityReportingServiceImpl])
 trait ActivityReportingService {
-  def alertsByProvider(provider: ActivityProvider, interval: Interval): Future[Seq[ActivityDocument]]
+  type ProviderCounts = Seq[(ActivityProvider, Int)]
 
   def alertsCountByProvider(provider: ActivityProvider, interval: Interval): Future[Int]
 
-  def alertsByProviders(providers: Map[ActivityProvider, Interval]): Future[Iterable[(ActivityProvider, Seq[ActivityDocument])]]
+  def alertsCountByProviders(providers: Map[ActivityProvider, Interval]): Future[ProviderCounts]
 
-  def alertsCountByProviders(providers: Map[ActivityProvider, Interval]): Future[Iterable[(ActivityProvider, Int)]]
+  def allAlertsCountByProviders(interval: Interval): Future[ProviderCounts]
 
-  def allAlertsByProviders(interval: Interval): Future[Iterable[(ActivityProvider, Seq[ActivityDocument])]]
-
-  def allAlertsCountByProviders(interval: Interval): Future[Iterable[(ActivityProvider, Int)]]
-
-  // activity as in non-alert activity
-  def activitiesByProvider(provider: ActivityProvider, interval: Interval): Seq[ActivityDocument]
-
-  def activitiesByProviders(providers: Map[ActivityProvider, Interval]): Future[Iterable[(ActivityProvider, Seq[ActivityDocument])]]
+  // We are not using the search function at all at the moment, but if one day we do,
+  // anything that could return a lot of results should use pagination, implemented
+  // using the search_after feature
+  // https://www.elastic.co/guide/en/elasticsearch/reference/current/search-request-search-after.html
 }
 
 class ActivityReportingServiceImpl @Inject()(
@@ -36,14 +33,6 @@ class ActivityReportingServiceImpl @Inject()(
   publisherDao: PublisherDao,
   @NamedDatabase("default") db: Database
 ) extends ActivityReportingService {
-  override def alertsByProvider(provider: ActivityProvider, interval: Interval): Future[Seq[ActivityDocument]] = {
-    val query = ActivityESSearchQuery(
-      provider_id = Some(provider.id),
-      publish_at = Some(interval),
-      isAlert = Some(true)
-    )
-    activityESService.search(query)
-  }
 
   override def alertsCountByProvider(provider: ActivityProvider, interval: Interval) = {
     val query = ActivityESSearchQuery(
@@ -54,23 +43,7 @@ class ActivityReportingServiceImpl @Inject()(
     activityESService.count(query)
   }
 
-  override def alertsByProviders(providers: Map[ActivityProvider, Interval]): Future[Iterable[(ActivityProvider, Seq[ActivityDocument])]] = {
-    import system.ThreadPools.elastic
-    Future.sequence(providers.map {
-      case (provider, interval) => (provider, this.alertsByProvider(provider, interval))
-    }.map {
-      case (provider, futureActivityDocuments) =>
-        for {
-          activityDocuments <- futureActivityDocuments
-        } yield (provider, activityDocuments)
-    }).map { result =>
-      result.toSeq.sortBy {
-        case (provider, _) => provider.displayName.getOrElse(provider.id)
-      }
-    }
-  }
-
-  override def alertsCountByProviders(providers: Map[ActivityProvider, Interval]) = {
+  override def alertsCountByProviders(providers: Map[ActivityProvider, Interval]): Future[ProviderCounts] = {
     import system.ThreadPools.elastic
     Future.sequence(providers.map {
       case (provider, interval) => (provider, this.alertsCountByProvider(provider, interval))
@@ -86,19 +59,7 @@ class ActivityReportingServiceImpl @Inject()(
     }
   }
 
-
-  override def allAlertsByProviders(interval: Interval): Future[Iterable[(ActivityProvider, Seq[ActivityDocument])]] = {
-    db.withConnection(implicit c => {
-      val providersWithInterval = publisherDao
-        .getAllProviders()
-        .map(ProviderRender.toActivityProvider)
-        .map((_, interval))
-        .toMap
-      this.alertsByProviders(providersWithInterval)
-    })
-  }
-
-  override def allAlertsCountByProviders(interval: Interval) = {
+  override def allAlertsCountByProviders(interval: Interval): Future[ProviderCounts] = {
     db.withConnection(implicit c => {
       val providersWithInterval = publisherDao
         .getAllProviders()
@@ -108,8 +69,4 @@ class ActivityReportingServiceImpl @Inject()(
       this.alertsCountByProviders(providersWithInterval)
     })
   }
-
-  override def activitiesByProvider(provider: ActivityProvider, interval: Interval) = ???
-
-  override def activitiesByProviders(providers: Map[ActivityProvider, Interval]) = ???
 }
