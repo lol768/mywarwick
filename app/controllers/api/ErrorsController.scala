bec8797cfdf7065c77715412f1ad7bf8c168e8f6
Kai Lan <k.lan@warwick.ac.uk>
2017-12-05 16:25:37 +0000

    NEWSTART-1264 guard it with Try

--- app/controllers/api/ErrorsController.scala
+++ app/controllers/api/ErrorsController.scala
@@ -9,6 +9,8 @@ import org.slf4j.{Logger, LoggerFactory}
 import play.api.libs.json.JsValue
 import play.api.mvc.Action
 
+import scala.util.Try
+
 @Singleton
 class ErrorsController extends MyController {
 
@@ -17,7 +19,7 @@ class ErrorsController extends MyController {
   def js = Action { implicit request =>
     request.body.asJson.flatMap(_.validate[Seq[Map[String, JsValue]]].asOpt).toSeq.flatten.foreach { error =>
       val message = error.get("message").map(_.as[String]).getOrElse("-")
-      val stacktrace = StructuredArguments.keyValue("stack_trace", error.get("stack").map(_.as[String]).getOrElse("-"))
+      val stacktrace = StructuredArguments.keyValue("stack_trace", Try(error.get("stack").map(_.as[String]).getOrElse("-")).getOrElse("-"))
       slf4jLogger.info(message, stacktrace)
     }
     Ok("")
