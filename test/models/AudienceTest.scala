aea999c3dd3e5d4805b9b3f66d54b9d78839d4d2
Kai Lan <k.lan@warwick.ac.uk>
2017-12-06 13:17:24 +0000

    NEWSTART-1267 update case class accordingly

--- test/models/AudienceTest.scala
+++ test/models/AudienceTest.scala
@@ -8,21 +8,21 @@ class AudienceTest extends BaseSpec {
   "Activity" should {
     "be able to tell usercode is invalid" in {
       var invalideUsercode = Usercode("1,bb")
-      Audience.isValidUsercode(invalideUsercode) mustBe false
+      Audience.helper.isLikelyValidUsercode(invalideUsercode) mustBe false
 
       invalideUsercode = Usercode("*jd0")
-      Audience.isValidUsercode(invalideUsercode) mustBe false
+      Audience.helper.isLikelyValidUsercode(invalideUsercode) mustBe false
 
       invalideUsercode = Usercode("abcsdf,skkdsf,ds233")
-      Audience.isValidUsercode(invalideUsercode) mustBe false
+      Audience.helper.isLikelyValidUsercode(invalideUsercode) mustBe false
     }
 
     "be able to tell usercode is valid" in {
       var valideUsercode = Usercode("u1574595")
-      Audience.isValidUsercode(valideUsercode) mustBe true
+      Audience.helper.isLikelyValidUsercode(valideUsercode) mustBe true
 
       valideUsercode = Usercode("u15745_95")
-      Audience.isValidUsercode(valideUsercode) mustBe true
+      Audience.helper.isLikelyValidUsercode(valideUsercode) mustBe true
     }
 
     "be able to tell usercodes are invalid" in {
@@ -31,7 +31,7 @@ class AudienceTest extends BaseSpec {
         Usercode("abcsdf,skkdsf,ds233"),
         Usercode("u1444444"),
       )
-      Audience.areValidUsercodes(invalid) mustBe false
+      Audience.helper.areValidUsercodes(invalid) mustBe false
     }
 
     "be able to tell usercodes are valid" in {
@@ -40,7 +40,7 @@ class AudienceTest extends BaseSpec {
         Usercode("abcsdf"),
         Usercode("u1444444"),
       )
-      Audience.areValidUsercodes(valid) mustBe true
+      Audience.helper.areValidUsercodes(valid) mustBe true
     }
 
   }
