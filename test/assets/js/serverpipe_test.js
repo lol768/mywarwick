07ae294492efd352736ff452805aa5031b347a41
Kai Lan <k.lan@warwick.ac.uk>
2017-12-13 17:14:27 +0000

    NEWSTART-1268 remove unneeded test case

--- test/assets/js/serverpipe_test.js
+++ test/assets/js/serverpipe_test.js
@@ -1,34 +1,17 @@
-import { addQsToUrl, appendTimeStampToQs } from 'serverpipe';
+import { addQsToUrl, generateTimeStampQsForUrl } from 'serverpipe';
 
 describe('serverpipe', () => {
 
   it('append querystring to url', () => {
 
-    const originalUrl = '/api/test';
+    const originalUrl = '/api/test?cat=white';
     const qs = {
       cake: 'salted',
       ts: 123,
     };
 
-    const expected = `/api/test?cake=salted&ts=123`;
+    const expected = `/api/test?cat=white&cake=salted&ts=123`;
     assert.equal(addQsToUrl(originalUrl, qs), expected);
   });
-
-  it('append timestamp to a given querystring', () => {
-
-    const qs = {
-      name: 'who',
-    };
-
-    const timeStamp = 123;
-    const result = appendTimeStampToQs(qs, timeStamp);
-    const expected = {
-      name: 'who',
-      ts: 123,
-    };
-
-    assert.deepEqual(result, expected);
-
-  })
 
 });
