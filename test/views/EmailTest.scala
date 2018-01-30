c5effee9f30f1d5a63488754edd5cc2548a5b22b
Nick Howes <n.howes@warwick.ac.uk>
2018-01-30 19:24:46 +0000

    NEWSTART-1299 Unit test

---
+++ test/views/EmailTest.scala
@@ -0,0 +1,29 @@
+package views
+
+import helpers.{BaseSpec, Fixtures}
+import org.joda.time.DateTime
+import org.scalatestplus.play.PlaySpec
+
+import org.scalatestplus.play._
+
+import play.api.mvc._
+import play.api.test._
+import play.api.test.Helpers._
+
+class EmailTest extends BaseSpec {
+
+  "email.scala.html" should {
+    "render title link" in {
+      val user = Fixtures.user.makeFoundUser()
+      val activity = Fixtures.activity.fromSave("1", Fixtures.activitySave.submissionDue)
+      val date = "2049"
+      val baseUrl = "https://my.warwick.invalid"
+      val optOutRoute = "https://options.invalid/optout"
+      val loggedInBefore = true
+      val result = views.html.email(user, activity, date, baseUrl, optOutRoute, loggedInBefore)
+      val content = contentAsString(result)
+      content must contain (s"""<a href="http://tabula.warwick.ac.uk">Coursework due</a>""")
+    }
+  }
+
+}
