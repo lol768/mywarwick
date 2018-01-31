5cd6f46c70a8972d41839aa3e02835b854d737bf
Kai Lan <k.lan@warwick.ac.uk>
2018-01-31 15:06:10 +0000

    NEWSTART-1293 resolve optin audience

--- app/services/AudienceService.scala
+++ app/services/AudienceService.scala
@@ -70,7 +70,7 @@ class AudienceServiceImpl @Inject()(
       case DepartmentAudience(code, subsets) => Future.sequence(subsets.map(subset =>
         makeResult(resolveDepartmentGroup(code, subset), subset)
       )).map(_.flatten)
-      case optIn: OptIn => makeResult(Future.successful(Nil))
+      case optIn: OptIn => makeResult(Future.successful(optInDao.getUsercodes(optIn)))
     }
   }
 
