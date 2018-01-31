64f964d067ff82d55ae2c05fae6684ca464b6e65
Kai Lan <k.lan@warwick.ac.uk>
2018-01-31 10:25:22 +0000

    NEWSTART-1300 updated the unit test so it checks these opt out links are rendered properly

--- test/views/EmailTest.scala
+++ test/views/EmailTest.scala
@@ -12,17 +12,37 @@ import play.api.test.Helpers._
 
 class EmailTest extends BaseSpec {
 
+  val user = Fixtures.user.makeFoundUser()
+  val activity = Fixtures.activity.fromSave("1", Fixtures.activitySave.submissionDue)
+  val date = "2049"
+  val baseUrl = "https://my.warwick.invalid"
+  val optOutRoute = "/optout"
+  val loggedInBefore = true
+
   "email.scala.html" should {
     "render title link" in {
-      val user = Fixtures.user.makeFoundUser()
-      val activity = Fixtures.activity.fromSave("1", Fixtures.activitySave.submissionDue)
-      val date = "2049"
-      val baseUrl = "https://my.warwick.invalid"
-      val optOutRoute = "/optout"
-      val loggedInBefore = true
       val result = views.html.email(user, activity, date, baseUrl, optOutRoute, loggedInBefore)
       val content = contentAsString(result)
-      content must include (s"""<a href="http://tabula.warwick.ac.uk">Coursework due</a>""")
+      content must include(s"""<a href="http://tabula.warwick.ac.uk">Coursework due</a>""")
+    }
+
+    "render title as text" in {
+      val result = views.html.email(user,
+        Fixtures.activity.fromSave("1", Fixtures.activitySave.submissionDueWithoutUrl),
+        date,
+        baseUrl,
+        optOutRoute,
+        loggedInBefore
+      )
+      val content = contentAsString(result)
+      content must include(s"""<p>Coursework due</p>""")
+    }
+
+    "render optout link" in {
+      var result = views.html.email(user, activity, date, baseUrl, optOutRoute, loggedInBefore)
+      contentAsString(result) must include(s"""<a href="https://my.warwick.invalid/optout">disable this</a>""")
+      result = views.html.email(user, activity, date, baseUrl, optOutRoute, !loggedInBefore)
+      contentAsString(result) must include(s"""<a href="https://my.warwick.invalid/optout">disable these emails</a>""")
     }
   }
 
