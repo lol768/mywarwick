e8208d216d69258a5b5bebdec41cf73c3ae448b5
Kai Lan <k.lan@warwick.ac.uk>
2018-02-06 10:32:23 +0000

    NEWSTART-1293 adding news category picker as a react component WIP

---
+++ app/assets/js/publish/components/NewsCategoryPicker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { AudiencePicker } from './AudiencePicker';
+
+export class NesCategoryPucker extends React.PureComponent {
+
+  static propTypes = {
+
+  }
+
+
+
+}
