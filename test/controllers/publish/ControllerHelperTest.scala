38e6510081bf2f32aafc3c5ea3efb680b1f07225
Kai Lan <k.lan@warwick.ac.uk>
2018-02-12 13:36:34 +0000

    NEWSTART-1315 adding a unit test for the change WIP

---
+++ test/controllers/publish/ControllerHelperTest.scala
@@ -0,0 +1,16 @@
+package controllers.publish
+
+import helpers.{BaseSpec, Fixtures}
+import models.ActivityRenderWithAudience
+
+class ControllerHelperTest extends BaseSpec {
+
+  "Controller helper" should {
+    "filter non-api activities with nonApiActivities" in {
+
+      val activities: Seq[ActivityRenderWithAudience] = Seq(
+      )
+    }
+  }
+
+}
