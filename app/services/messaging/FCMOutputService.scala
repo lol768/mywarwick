0ee320c5870fac3998a01ae71cfdc297b1f31e8a
Matthew Jones <matthew.jones@warwick.ac.uk>
2018-02-14 14:46:02 +0000

    NEWSTART-1319 - Use v1 FCM API

--- app/services/messaging/FCMOutputService.scala
+++ app/services/messaging/FCMOutputService.scala
@@ -1,6 +1,9 @@
 package services.messaging
 
+import java.io.FileInputStream
+
 import actors.MessageProcessing.ProcessingResult
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.inject.Inject
 import models.MessageSend
 import models.Platform.Google
@@ -11,7 +14,7 @@ import play.api.libs.ws.WSClient
 import services.dao.PushRegistrationDao
 import system.Logging
 import warwick.sso.Usercode
-
+import collection.JavaConverters._
 import scala.concurrent.Future
 
 
@@ -26,8 +29,21 @@ class FCMOutputService @Inject()(
 
   private val ARROW_EMOJI = "↗️"
 
-  val apiKey = configuration.getOptional[String]("mywarwick.fcm.apiKey")
-    .getOrElse(throw new IllegalStateException("Missing FCM API key - set mywarwick.fcm.apiKey"))
+  private val FCMProjectId = configuration.getOptional[String]("mywarwick.fcm.projectId")
+    .getOrElse(throw new IllegalStateException("Missing FCM API key - set mywarwick.fcm.projectId"))
+
+  private val FCMServiceAccountKeyPath = configuration.getOptional[String]("mywarwick.fcm.serviceAccountKeyPath")
+    .getOrElse(throw new IllegalStateException("Missing FCM API key - set mywarwick.fcm.serviceAccountKeyPath"))
+
+  private val FCMScope = "https://www.googleapis.com/auth/firebase.messaging"
+
+  private lazy val googleCredential: GoogleCredential =
+    GoogleCredential.fromStream(new FileInputStream(FCMServiceAccountKeyPath)).createScoped(Seq(FCMScope).asJava)
+
+  private def getFCMAccessToken: String = {
+    googleCredential.refreshToken()
+    googleCredential.getAccessToken
+  }
 
   def send(message: MessageSend.Heavy): Future[ProcessingResult] = {
     val usercode = message.user.usercode
@@ -45,33 +61,47 @@ class FCMOutputService @Inject()(
 
   def sendNotification(message: MessageSend.Heavy)(token: String): Future[Unit] = {
     val body = Json.obj(
-      "to" -> token,
-      "notification" -> Json.obj(
-        "title" -> JsString(message.activity.url.map(_ => s"${message.activity.title} $ARROW_EMOJI").getOrElse(message.activity.title)),
-        "body" -> message.activity.text,
-        "sound" -> "default"
+      "message" -> Json.obj(
+        "token" -> token,
+        "notification" -> Json.obj(
+          "title" -> JsString(message.activity.url.map(_ => s"${message.activity.title} $ARROW_EMOJI").getOrElse(message.activity.title)),
+          "body" -> message.activity.text
+        ),
+        "android" -> Json.obj(
+          "notification" -> Json.obj(
+            "sound" -> "default"
+          )
+        )
       )
     )
 
-    ws.url("https://fcm.googleapis.com/fcm/send")
+    val FCMToken = getFCMAccessToken
+
+    ws.url(s"https://fcm.googleapis.com/v1/projects/$FCMProjectId/messages:send")
       .addHttpHeaders(
-        "Authorization" -> s"key=$apiKey",
+        "Authorization" -> s"Bearer $FCMToken",
         "Content-Type" -> "application/json"
       )
       .post(body)
       .map { response =>
-        val errors = (response.json \ "results" \\ "error").map(_.as[String])
-
-        if (errors.contains("NotRegistered")) {
-          logger.info(s"Received NotRegistered FCM error, removing token=$token")
-          db.withConnection { implicit c =>
-            pushRegistrationDao.removeRegistration(token)
+        (response.json \ "error_code").validateOpt[String].fold(
+          errors => {
+            logger.error(s"Could not parse JSON result from FCM:")
+            errors.foreach { case (path, validationErrors) =>
+              logger.error(s"$path: ${validationErrors.map(_.message).mkString(", ")}")
+            }
+          },
+          e => e.foreach { error =>
+            if (error == "UNREGISTERED") {
+              logger.info(s"Received UNREGISTERED FCM error, removing token=$token")
+              db.withConnection { implicit c =>
+                pushRegistrationDao.removeRegistration(token)
+              }
+            } else {
+              logger.warn(s"FCM error: $error")
+            }
           }
-        }
-
-        if (errors.nonEmpty && !errors.forall(_ == "NotRegistered")) {
-          logger.warn("FCM errors: " + errors.mkString(", "))
-        }
+        )
       }
   }
 
