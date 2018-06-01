9fc12973795fb29863e12c26bdabe52eb235a0d4
Kai Lan <k.lan@warwick.ac.uk>
2018-06-01 10:25:24 +0100

    NEWSTART-1489 update frontend accordingly

--- app/assets/js/publish/components/AudienceIndicator.js
+++ app/assets/js/publish/components/AudienceIndicator.js
@@ -117,7 +117,7 @@ export class AudienceIndicator extends React.PureComponent {
                       return (
                         <div key={key}>
                           All residents
-                          of {displayName}: {getCount([`ResidenceAudience(${displayName})`], halls)}
+                          of {displayName}: {getCount([`ResidenceAudience(${displayName.replace(' ','')})`], halls)}
                         </div>
                       );
                     }) : null;
