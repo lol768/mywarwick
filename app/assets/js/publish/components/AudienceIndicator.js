28895c15c8bdbe0db64381e081ede44b2925ddf5
Kai Lan <k.lan@warwick.ac.uk>
2018-06-01 10:27:29 +0100

    NEWSTART-1489 lint

--- app/assets/js/publish/components/AudienceIndicator.js
+++ app/assets/js/publish/components/AudienceIndicator.js
@@ -117,7 +117,7 @@ export class AudienceIndicator extends React.PureComponent {
                       return (
                         <div key={key}>
                           All residents
-                          of {displayName}: {getCount([`ResidenceAudience(${displayName.replace(' ','')})`], halls)}
+                          of {displayName}: {getCount([`ResidenceAudience(${displayName.replace(' ', '')})`], halls)}
                         </div>
                       );
                     }) : null;
