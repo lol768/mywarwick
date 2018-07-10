62348b49b6ba3eaf7348c9201bce4471fd15cbbe
Nick Kaijaks <Nick.Kaijaks@warwick.ac.uk>
2018-07-10 16:53:30 +0100

    NEWSTART-1074 Webpacked admin-bundle.js with fetch/promise polyfills

---
+++ app/assets/js/admin.js
@@ -0,0 +1,52 @@
+/* eslint-env browser */
+
+import $ from 'jquery';
+import fetch from 'isomorphic-fetch';
+import promisePolyfill from 'es6-promise/lib/es6-promise/polyfill';
+
+promisePolyfill();
+
+const $clientReportContainer = $('.client-report');
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function pollForClientReport() {
+  const start = $clientReportContainer.data('start') || '';
+  const end = $clientReportContainer.data('end') || '';
+  const cacheTimeout = $clientReportContainer.data('cachelife') || 'a short while';
+  const pollTimeoutInMins = 10;
+  const pollTimeoutInMillis = 60000 * pollTimeoutInMins;
+  const pollIntervalInMillis = 1500;
+  const endTime = Number(new Date()) + pollTimeoutInMillis;
+
+  return fetch(`/admin/reports/clients/${start}/${end}`, {
+    credentials: 'include',
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.text();
+      } else if (Number(new Date()) > endTime) {
+        throw new Error('timeout');
+      }
+
+      return sleep(pollIntervalInMillis);
+    })
+    .then((result) => {
+      if (typeof result === 'string') {
+        $clientReportContainer.html(result);
+      } else {
+        pollForClientReport();
+      }
+    })
+    .catch(() => {
+      $clientReportContainer.html(`<p class="alert alert-warning">The report has not been built after ${pollTimeoutInMins} minutes. Please <i>Search</i> again later to see if the report is available. Reports are cached for ${cacheTimeout}.</p>`);
+    });
+}
+
+if ($clientReportContainer.length) {
+  pollForClientReport();
+}
