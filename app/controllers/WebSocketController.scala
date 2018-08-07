0c857b571c857268b6f0dd8699dc4174d0a5844a
Nick Howes <n.howes@warwick.ac.uk>
2018-08-07 11:25:00 +0100

    NEWSTART-1566 Check Origin

--- app/controllers/WebSocketController.scala
+++ app/controllers/WebSocketController.scala
@@ -1,13 +1,13 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-
 import actors.{PubSubActor, WebSocketActor}
 import akka.actor.{ActorRef, ActorSystem}
 import akka.stream.Materializer
+import play.api.Configuration
 import play.api.libs.json.JsValue
 import play.api.libs.streams.ActorFlow
-import play.api.mvc.WebSocket
+import play.api.mvc.{Results, WebSocket}
 import services.SecurityService
 import system.{AppMetrics, Logging}
 import warwick.sso.LoginContext
@@ -20,20 +20,27 @@ class WebSocketController @Inject()(implicit
   security: SecurityService,
   metrics: AppMetrics,
   system: ActorSystem,
-  mat: Materializer
+  mat: Materializer,
+  config: Configuration,
 ) extends Logging {
 
   import security._
+
+  private val origin: String = config.get[String]("mywarwick.rootUrl")
 
   // This actor lives as long as the controller
   private val pubSubActor = system.actorOf(PubSubActor.props())
 
   def socket: WebSocket = WebSocket.acceptOrResult[JsValue, JsValue] { request =>
-    SecureWebsocket(request) { loginContext: LoginContext =>
-      val who = loginContext.user.map(_.usercode).getOrElse("nobody")
-      logger.info(s"WebSocket opening for $who")
-      val flow = ActorFlow.actorRef(out => WebSocketActor.props(loginContext, metrics.websocketTracker(), pubSubActor, out))
-      Future.successful(Right(flow))
+    if (request.headers("Origin") == origin) {
+      SecureWebsocket(request) { loginContext: LoginContext =>
+        val who = loginContext.user.map(_.usercode).getOrElse("nobody")
+        logger.info(s"WebSocket opening for $who")
+        val flow = ActorFlow.actorRef(out => WebSocketActor.props(loginContext, metrics.websocketTracker(), pubSubActor, out))
+        Future.successful(Right(flow))
+      }
+    } else {
+      Future.successful(Left(Results.Unauthorized("Bad Origin")))
     }
   }
 
