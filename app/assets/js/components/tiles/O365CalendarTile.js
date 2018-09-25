946509af7be1982ecd6e616680837211dcdb66f3
Nic Duke <n.duke@warwick.ac.uk>
2018-09-25 09:44:14 +0100

    NEWSTART-1580 have O365CalendarTile external links open in new window

--- app/assets/js/components/tiles/O365CalendarTile.js
+++ app/assets/js/components/tiles/O365CalendarTile.js
@@ -1,13 +1,31 @@
+/* eslint-env browser */
 import React from 'react';
 import AgendaTile from './agenda/AgendaTile';
 
+const WEBCAL_URL = 'http://webcalendar.warwick.ac.uk/';
+
 export default class O365CalendarTile extends AgendaTile {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    window.open(WEBCAL_URL);
+  }
+
   contentOrDefault(contentFunction) {
     if (this.isEmpty()) {
       return (
         <div>
           <p>{this.props.content.defaultText}</p>
-          <a className="text--dotted-underline" href="http://webcalendar.warwick.ac.uk/">
+          <a
+            className="text--dotted-underline"
+            role="button"
+            tabIndex={0}
+            title="Open calendar"
+            onClick={this.handleClick}
+          >
             Open calendar
           </a>
         </div>
@@ -19,13 +37,14 @@ export default class O365CalendarTile extends AgendaTile {
 
   modalMoreButton() {
     return (
-      <a
+      <button
         type="button"
         className="btn btn-default"
-        href="http://webcalendar.warwick.ac.uk/"
+        title="Open calendar"
+        onClick={this.handleClick}
       >
         Open calendar
-      </a>
+      </button>
     );
   }
 }
